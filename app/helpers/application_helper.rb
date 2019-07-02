17846eeb6e3b1fc7ed20a7b4d288e8a789a61214
Mitchel <mitchel_jager@hotmail.com>
2019-07-02 11:47:19 +0200

    Added youtube embed in Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,10 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer)
 
-    markdown.render(text).html_safe
+    iframe = '<div class="video"><iframe class="video__iframe" width="560" height="315" src="https://www.youtube.com/embed/\\1" frameborder="0" allowfullscreen></iframe></div>'
+
+    content = markdown.render(text)
+    content.gsub(/\[youtube\s+(.*?)\]/, iframe).html_safe
   end
 
   def to_slug(string)
