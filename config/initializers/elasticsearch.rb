1c522b1108f86894a2bd920cbab5922927b88066
Mitchel <mitchel_jager@hotmail.com>
2019-07-03 19:17:35 +0200

    Set Bonsai URL in initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,14 +1,1 @@
-config = {
-
-host: "http://localhost:9200/",
-
-transport_options: {
-
-request: { timeout: 5 }
-  }
-}
-if File.exists?("config/elasticsearch.yml")
-
-config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
-end
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV["BONSAI_URL"]
