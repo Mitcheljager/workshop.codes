16f0c203b57db58648e9f44ef4d6f70396f87932
Mitchel <mitchel_jager@hotmail.com>
2019-11-06 13:43:37 +0100

    Remove private snippets from sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,7 +1,7 @@
 class SitemapsController < ApplicationController
   def sitemap
     @posts = Post.all
-    @snippets = Snippet.all
+    @snippets = Snippet.where(private: 0)
 
     respond_to do |format|
       format.xml
