a68ea8375c7e4bd275f305b2121dc0cd9e77a381
Mitchel <mitchel_jager@hotmail.com>
2020-01-03 00:59:23 +0100

    Use remote calls for search when possible

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
   def index
     unless params[:query].empty?
-      redirect_to build_filter_path(:search, params[:query].gsub(".", ""))
+      respond_to do |format|
+        format.js { redirect_to build_filter_path(:search, params[:query].gsub(".", "")) }
+        format.html { redirect_to build_filter_path(:search, params[:query].gsub(".", "")) }
+      end
     else
       redirect_back fallback_location: root_path
     end
