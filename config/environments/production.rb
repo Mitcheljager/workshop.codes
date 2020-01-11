784e780303de78e4dd0f077b608c281e0d310573
Mitchel <mitchel_jager@hotmail.com>
2020-01-11 18:54:50 +0100

    Attempt at fixing mail delivery on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,10 +67,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "overwatch-workshop_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://www.workshop.codes" }
-  Rails.application.routes.default_url_options[:host] = "https://www.workshop.codes"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "workshop.codes",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.sendgrid.net",
-  port: 587,
-  domain: "workshop.codes",
-  user_name: ENV["SENDGRID_USERNAME"],
-  password: ENV["SENDGRID_PASSWORD"],
-  authentication: :plain,
-  enable_starttls_auto: true
-}
