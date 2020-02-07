7c3a5b0c46ac575f66602b49efc3addbadfce8bf
Mitchel <mitchel_jager@hotmail.com>
2020-02-07 17:59:05 +0100

    Prevent js error when no timeago elements are present

--- app/assets/javascripts/timeago.js
+++ app/assets/javascripts/timeago.js
@@ -1,5 +1,5 @@
 document.addEventListener("turbolinks:load", function() {
   const elements = document.querySelectorAll("[data-role='timeago']")
 
-  timeago.render(elements)
+  if (elements.length) timeago.render(elements)
 })
