e5eafb2c81a3afee4f77300702412c18100344cd
Mitchel <mitchel_jager@hotmail.com>
2020-02-26 23:06:17 +0100

    Use correct redis gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, ENV["FLY_REDIS_CACHE_URL"]
+  config.cache_store = :redis_cache_store, { url: ENV["FLY_REDIS_CACHE_URL"] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
