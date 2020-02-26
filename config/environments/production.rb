1819782e7ac4e9896f1a697ae2ef18e6dcb86c1d
Mitchel <mitchel_jager@hotmail.com>
2020-02-26 22:45:35 +0100

    Use redis as cache store on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, ENV["FLY_REDIS_CACHE_URL"]
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
