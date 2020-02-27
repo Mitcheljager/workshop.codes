73125463ea54142ef5be090d2b01d420f255f35f
Mitchel <mitchel_jager@hotmail.com>
2020-02-27 01:07:16 +0100

    Revert "Specify utf_8 encoding"
    This reverts commit b913ebdb458084819a837fc7fa54becad35edc62.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-
 # Initialize the Rails application.
 Rails.application.initialize!
