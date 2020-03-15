0a255ade67a66d87f3d5d0c27412a6a4fe8b42d4
Mitchel <mitchel_jager@hotmail.com>
2020-03-15 19:47:46 +0100

    Remove caching of favorite button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.includes(:favorites, :notifications, :remember_tokens).find(session[:user_id])
     else
       @current_user = nil
     end
