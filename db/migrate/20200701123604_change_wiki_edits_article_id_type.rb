36a1cef024c3c2afe465459567cd106ba892ebee
Mitchel <mitchel_jager@hotmail.com>
2020-07-01 14:44:46 +0200

    set "using" in migration for pg

--- db/migrate/20200701123604_change_wiki_edits_article_id_type.rb
+++ db/migrate/20200701123604_change_wiki_edits_article_id_type.rb
@@ -1,5 +1,5 @@
 class ChangeWikiEditsArticleIdType < ActiveRecord::Migration[6.0]
   def change
-    change_column :wiki_edits, :article_id, :integer
+    change_column :wiki_edits, :article_id, :integer, using: "article_id::integer"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_123603) do
+ActiveRecord::Schema.define(version: 2020_07_01_123604) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
