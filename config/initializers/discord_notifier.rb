98c9830caf43de35df69f0814eb83247e244c04f
Mitchel <mitchel_jager@hotmail.com>
2020-07-28 20:57:07 +0200

    Set default discord avatar

--- config/initializers/discord_notifier.rb
+++ config/initializers/discord_notifier.rb
@@ -1,7 +1,7 @@
 Discord::Notifier.setup do |config|
   config.url = ENV["DISCORD_NOTIFICATIONS_WEBHOOK_URL"]
   config.username = "Workshop.codes Notifications"
-  config.avatar_url = ""
+  config.avatar_url = "https://cdn.discordapp.com/app-icons/695034676880080956/958269ce0daaf15759362b629d14fd55.png"
 
   # Defaults to `false`
   config.wait = true
