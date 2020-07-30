b9d536c8c9165bf7d34f8c65370e184097238386
Mitchel <mitchel_jager@hotmail.com>
2020-07-30 02:33:18 +0200

    Added 500 error to custom error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,4 +6,8 @@ class ErrorsController < ApplicationController
   def unacceptable
     render "unacceptable", status: 422
   end
+
+  def internal_error
+    render "internal_error", status: 500
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unacceptable"
+  get "/500", to: "errors#internal_error"
 
   get "sitemap", to: "sitemaps#sitemap"
 
