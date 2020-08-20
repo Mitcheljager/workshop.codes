f49581c53e654105fccbc2149badc4080617177d
Mitchel <mitchel_jager@hotmail.com>
2020-08-20 03:46:53 +0200

    Fallback when image is not found for whatever reason

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,22 @@
 module PostsHelper
   def url_for_post_thumbnail(post)
-    if post.image_order.present? && JSON.parse(post.image_order).length
+    url = ""
+
+    if post.image_order.present? && JSON.parse(post.image_order || "[]").length
       image = post.images.find_by_blob_id(JSON.parse(post.image_order).first)
 
-      url_for image.variant(quality: 85, resize_to_fill: [120, 68]).processed
-    else
+      return unless image
+
+      url = url_for image.variant(quality: 85, resize_to_fill: [120, 68]).processed
+    end
+
+    unless url.present?
       random_with_seed = Random.new(post.id).rand(JSON.parse(post.maps).length)
       map = maps.find { |m| m["name"] == JSON.parse(post.maps)[random_with_seed - 1] }
 
-      "maps/small/#{ map["slug"] }.jpg"
+      url = "maps/small/#{ map["slug"] }.jpg"
     end
+
+    return url
   end
 end
