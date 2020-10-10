09200442b14ecec0c4b46cb91ec4a316c660343c
Mitchel <mitchel_jager@hotmail.com>
2020-10-10 20:42:10 +0200

    Fix tests

--- test/controllers/while_you_waits_controller_test.rb
+++ test/controllers/while_you_waits_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
+require 'capybara/rails'
+require 'capybara/minitest'
 
 class WhileYouWaitsControllerTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  setup do
+    @posts = posts
+    @post = posts(:post_one)
+
+    @while_you_waits = while_you_waits
+  end
+
   test "should get index" do
-    get while_you_wait_url
-    assert_response :success
+    visit while_you_wait_url
+
+    assert page.has_content? "While You Wait"
+
+    assert page.has_content? posts(:post_one).title
+    assert page.has_content? posts(:post_two).title
   end
 end
