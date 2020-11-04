1491887d5a61d8c7bff4ea60dd6f275e4ba00256
Mitchel <mitchel_jager@hotmail.com>
2020-11-04 18:05:11 +0100

    remove description and snippet from filter json

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -22,7 +22,7 @@ class FilterController < ApplicationController
       format.js { render "posts/infinite_scroll_posts" }
       format.json {
         set_request_headers
-        render json: @posts
+        render json: @posts.as_json(except: [:description, :snippet])
       }
     end
   end
