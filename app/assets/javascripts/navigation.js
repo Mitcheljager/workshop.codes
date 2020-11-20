9dcc32579aa322f1679816f6fd7f1ff5a1c916a3
Mitchel <mitchel_jager@hotmail.com>
2020-11-20 21:02:35 +0100

    Overhaul of the overall design of the header and nearby elements

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -3,12 +3,24 @@ document.addEventListener("turbolinks:load", function() {
 
   elements.forEach((element) => element.removeEventListener("click", toggleNavigation))
   elements.forEach((element) => element.addEventListener("click", toggleNavigation))
+
+  const searchElement = document.querySelector("[data-action='toggle-search']")
+  searchElement.removeEventListener("click", toggleSearch)
+  searchElement.addEventListener("click", toggleSearch)
 })
 
 function toggleNavigation(event) {
   event.preventDefault()
 
   const navigationElement = document.querySelector("[data-role='navigation']")
-
   navigationElement.classList.toggle("header__content--is-active")
+}
+
+function toggleSearch(event) {
+  event.preventDefault()
+
+  const navigationElement = document.querySelector("[data-role='search']")
+  navigationElement.classList.toggle("header__search--is-active")
+
+  navigationElement.querySelector("input[name='query']").focus()
 }
