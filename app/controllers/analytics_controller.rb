ec6c5655a0b80725687ee6ba2ea40df435061ecd
Mitchel <mitchel_jager@hotmail.com>
2021-01-12 11:27:25 +0100

    Remove hourly analytics, make analytics max 6 months.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -12,12 +12,6 @@ class AnalyticsController < ApplicationController
       counts = create_daily_counts(:visit)
     elsif params[:type] == "daily-listings"
       counts = create_daily_counts(:listing)
-    elsif params[:type] == "hourly-copies"
-      counts = create_hourly_counts("Copy Code")
-    elsif params[:type] == "hourly-views"
-      counts = create_hourly_counts("Posts Visit")
-    elsif params[:type] == "hourly-listings"
-      counts = create_hourly_counts("Listing")
     end
 
     render json: counts, layout: false
@@ -26,7 +20,7 @@ class AnalyticsController < ApplicationController
   def user
     date_counts = []
     if current_user.posts.any?
-      latest_date = [current_user.posts.first.created_at.strftime("%Y-%m-%d"), Statistic.first.created_at.strftime("%Y-%m-%d")].max
+      latest_date = [current_user.posts.first.created_at.strftime("%Y-%m-%d"), 6.months.ago.strftime("%Y-%m-%d")].max
 
       (Date.parse(latest_date)...DateTime.now).each do |date|
         date_counts << { date: date.strftime("%Y-%m-%d"), value: 0 }
@@ -63,21 +57,14 @@ class AnalyticsController < ApplicationController
   private
 
   def create_date_count
+    latest_date = [@post.created_at.strftime("%Y-%m-%d"), 6.months.ago.strftime("%Y-%m-%d")].max
+
     date_counts = []
-    (Date.parse(@post.created_at.strftime("%Y-%m-%d %H:00"))...DateTime.now).each do |date|
+    (Date.parse(latest_date)...DateTime.now).each do |date|
       date_counts << { date: date.strftime("%Y-%m-%d %H:00"), value: 0 }
     end
 
     return date_counts
-  end
-
-  def create_hour_count
-    hour_counts = []
-    (1.week.ago.to_i .. DateTime.now.to_i).step(1.hour) do |date|
-      hour_counts << { date: Time.at(date).strftime("%Y-%m-%d %H:00"), value: 0 }
-    end
-
-    return hour_counts
   end
 
   def create_daily_counts(type)
@@ -86,17 +73,6 @@ class AnalyticsController < ApplicationController
     daily.group_by { |x| (x.on_date).to_date.strftime("%Y-%m-%d %H:00") }.each do |date, values|
       this = counts.detect { |d| d[:date] == date }
       this[:value] = values.map { |h| h[:value] }.sum if this.present?
-    end
-
-    return counts
-  end
-
-  def create_hourly_counts(type)
-    counts = create_hour_count
-    hourly = Rails.env.development? ? Ahoy::Event.where(name: type).distinct.last(10) : Ahoy::Event.where_event(type, id: @post.id).distinct
-    hourly.group_by { |x| x["time"].strftime("%Y-%m-%d %H:00") }.each do |date, values|
-      this = counts.detect { |d| d[:date] == date }
-      this[:value] = values.map(&:visit_id).uniq.count if this.present?
     end
 
     return counts
