e7645d2212b0bc81d8f103a9f49d703b993cad04
Mitchel <mitchel_jager@hotmail.com>
2021-01-15 20:42:04 +0100

    Show unlisted posts in collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,7 @@
 class Collection < ApplicationRecord
   belongs_to :user
 
-  has_many :posts, -> { select(:id, :title, :code, :ptr, :image_order, :maps, :user_id, :categories, :updated_at ) }
+  has_many :posts, -> { select(:id, :title, :code, :ptr, :image_order, :maps, :user_id, :categories, :updated_at, :last_revision_created_at ) }
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 end
