b02916d991745e391d443dfc67cc411480f3ce9b
Mitchel <mitchel_jager@hotmail.com>
2021-01-15 21:18:34 +0100

    Add addtional cache-control headers for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.public_file_server.headers = {
-    "Cache-Control" => "public, s-maxage=31536000, max-age=15552000",
-    "Expires" => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+    "Cache-Control" => "public, max-age=#{30.days.to_i}"
   }
 
   # Compress JavaScripts and CSS.
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( form.js uploader.js chart.js form.scss fonts.scss )
+
+Rails.application.config.public_file_server.headers = {
+  "Cache-Control" => "public, max-age=#{ 30.days.to_i }"
+}
