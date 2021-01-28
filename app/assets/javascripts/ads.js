361d7bd841ad8caedfdce9f7cd62d60e1cef9af3
Mitchel <mitchel_jager@hotmail.com>
2021-01-28 14:53:36 +0100

    Fix error when loading ads via infinite scroll

--- app/assets/javascripts/ads.js
+++ app/assets/javascripts/ads.js
@@ -4,8 +4,8 @@ function renderAds() {
   const elements = document.querySelectorAll(".adsbygoogle")
 
   elements.forEach(element => {
-    if (window.getComputedStyle(element.closest("div")).display === "none") return
     if (element.dataset.adsbygoogleStatus == "done") return
+    if (window.getComputedStyle(element.closest("div")).display === "none") return
 
     (adsbygoogle = window.adsbygoogle || []).push({})
   })
