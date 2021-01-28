61e99155dbd955d6708e0a1179f31507c194107e
Mitchel <mitchel_jager@hotmail.com>
2021-01-28 18:01:01 +0100

    No longer stop ads from rendering if they are hidden

--- app/assets/javascripts/ads.js
+++ app/assets/javascripts/ads.js
@@ -4,9 +4,6 @@ function renderAds() {
   const elements = document.querySelectorAll(".adsbygoogle")
 
   elements.forEach(element => {
-    if (element.dataset.adsbygoogleStatus == "done") return
-    if (window.getComputedStyle(element.closest("div")).display === "none") return
-
     (adsbygoogle = window.adsbygoogle || []).push({})
   })
 }
