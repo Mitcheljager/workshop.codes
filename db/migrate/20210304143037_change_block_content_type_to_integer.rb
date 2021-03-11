916e9dca512fc72dc44ef3f6aad366966189823c
Mitchel <mitchel_jager@hotmail.com>
2021-03-11 18:24:03 +0100

    State `using` in migration for PG

--- db/migrate/20210304143037_change_block_content_type_to_integer.rb
+++ db/migrate/20210304143037_change_block_content_type_to_integer.rb
@@ -1,9 +1,9 @@
 class ChangeBlockContentTypeToInteger < ActiveRecord::Migration[6.1]
   def up
-    change_column :blocks, :content_type, :integer, default: 0
+    change_column :blocks, :content_type, :integer, using: "content_type::integer", default: 0
   end
 
   def down
-    change_column :blocks, :content_type, :string
+    change_column :blocks, :content_type, :string, using: "content_type::string"
   end
 end
