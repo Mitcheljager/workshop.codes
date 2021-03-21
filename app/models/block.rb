6aa17d59b42fb07e00985ef5d188498c6e47e3a5
Mitchel <mitchel_jager@hotmail.com>
2021-03-21 20:01:24 +0100

    Validate block image size in model

--- app/models/block.rb
+++ app/models/block.rb
@@ -10,4 +10,6 @@ class Block < ApplicationRecord
   validates :name, presence: true
   validates :content_type, presence: true
   validates :user_id, presence: true
+  validates :images, content_type: ["image/png", "image/jpg", "image/jpeg"],
+                     size: { less_than: 2.megabytes }
 end
