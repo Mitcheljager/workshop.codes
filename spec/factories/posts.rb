671a57b56ed0a51b413bafca33294755ddb4209d
CactusPuppy <puppycactus@gmail.com>
2021-05-20 23:40:38 -0700

    Bump testing related gems to allow for continued test development in new environment

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     title { Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4) }
     code { Faker::Alphanumeric.alphanumeric(number: 5).upcase }
-    user_id { Faker::Number.number(digits: 1) }
+    user
     description { Faker::Markdown.sandwich(sentences: 6, repeat: 3) }
     categories { ApplicationHelper.categories.collect{ |a| a["en"] }.sample(2) }
     heroes { ApplicationHelper.heroes.collect{ |a| a["en"] }.sample(3) }
@@ -12,6 +12,5 @@ FactoryBot.define do
     hotness { Faker::Number.between(from: 1, to: 200) }
     min_players { Faker::Number.between(from: 1, to: 12) }
     max_players { Faker::Number.between(from: min_players, to: 12) }
-    user
   end
 end
