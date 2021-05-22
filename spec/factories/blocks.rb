52ddb4688e1407ce57490229c98afbeb1b5e2b43
CactusPuppy <puppycactus@gmail.com>
2021-05-22 00:40:41 -0700

    Make block factory output valid blocks

--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :block do
-    content_type { "MyString" }
+    content_type { "profile" }
     properties { "MyText" }
     content_id { 1 }
     position { 1 }
+    name { Faker::Name.first_name }
+    user
   end
 end
