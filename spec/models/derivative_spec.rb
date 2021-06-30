d64f988bfdea2800fe0098663c63c9e5ad676b1f
CactusPuppy <puppycactus@gmail.com>
2021-06-29 23:36:59 -0700

    Add specs to cover ActiveRecord validations

--- spec/models/derivative_spec.rb
+++ spec/models/derivative_spec.rb
@@ -16,6 +16,12 @@ RSpec.describe Derivative, type: :model do
         expect(derivation).not_to be_valid
       end
 
+      it "doesn't allow a post to derive from its own import code" do
+        deriv = build(:derivative)
+        deriv.source_code = deriv.derivation.code
+        expect(deriv).not_to be_valid
+      end
+
       it "allows a post to derive from another post" do
         expect(build(:derivative)).to be_valid
       end
@@ -28,6 +34,39 @@ RSpec.describe Derivative, type: :model do
         deriv2.source = deriv1.source
         deriv2.derivation = deriv1.derivation
         expect(deriv2).not_to be_valid
+      end
+
+      it "doesn't allow multiple derivations to exist for any given post and source import code" do
+        deriv1 = create(:derivative)
+        deriv1.source = nil
+        deriv1.source_code = "CODE1"
+        deriv1.save!
+        expect(deriv1).to be_valid
+
+        deriv2 = build(:derivative)
+        deriv2.source = nil
+        deriv2.derivation = deriv1.derivation
+        deriv2.source_code = deriv1.source_code
+        expect(deriv2).not_to be_valid
+      end
+
+      it "allows multiple derivations from a non-existent post" do
+        post1 = create(:post)
+        post2 = create(:post)
+
+        deriv1 = build(:derivative)
+        deriv1.source = nil
+        deriv1.source_code = "ABCDE"
+        deriv1.derivation = post1
+        expect(deriv1).to be_valid
+        deriv1.save!
+
+        deriv2 = build(:derivative)
+        deriv2.source = nil
+        deriv2.source_code = "ABCDE"
+        deriv2.derivation = post2
+        expect(deriv2).to be_valid
+        deriv2.save!
       end
     end
   end
