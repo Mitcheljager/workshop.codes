7f2086c870e3634bebcbf41aa085abd4f1e414e9
CactusPuppy <puppycactus@gmail.com>
2021-07-11 21:31:05 -0700

    Prevent unnecessary rendering for users

--- app/javascript/src/components/form/Derivatives.svelte
+++ app/javascript/src/components/form/Derivatives.svelte
@@ -61,24 +61,26 @@
     </label>
   </div>
 
-  <div class="form-group mt-1/4" hidden={!showDerivative}>
-    <div class="form-hint text-left">
-      Enter the import code(s) which your mode uses. You can enter up to {maxCodes} codes.
-      <br />
-      <strong>Separate import codes with a comma (<code>,</code>).</strong>
-    </div>
+  {#if showDerivative}
+    <div class="form-group mt-1/4">
+      <div class="form-hint text-left">
+        Enter the import code(s) which your mode uses. You can enter up to {maxCodes} codes.
+        <br />
+        <strong>Separate import codes with a comma (<code>,</code>).</strong>
+      </div>
 
-    <Tags
-      name="derivatives"
-      placeholder="CODE1,CODE2,etc."
-      fillValues={currentSources}
-      hidden={!showDerivative}
-      allowSpace={false}
-      onlyAlphanumeric={true}
-      onlyCaps={true}
-      tagLimit={maxCodes}
-      useAutoComplete={true}
-      fetchAutoCompleteValues={handleAutoCompleteRequest}
-    />
-  </div>
+      <Tags
+        name="derivatives"
+        placeholder="CODE1,CODE2,etc."
+        fillValues={currentSources}
+        hidden={!showDerivative}
+        allowSpace={false}
+        onlyAlphanumeric={true}
+        onlyCaps={true}
+        tagLimit={maxCodes}
+        useAutoComplete={true}
+        fetchAutoCompleteValues={handleAutoCompleteRequest}
+      />
+    </div>
+  {/if}
 </div>
