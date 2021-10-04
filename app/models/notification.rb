1151717ad7cec3441a55ceda7db6b9c13237c3c2
CactusPuppy <puppycactus@gmail.com>
2021-10-03 22:36:15 -0700

    Create notification for source post user whenever a derivative is created

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :user
 
-  enum content_type: { comment: 0, comment_reply: 1, will_expire: 2, has_expired: 3, earned_badge: 4 }
+  enum content_type: { comment: 0, comment_reply: 1, will_expire: 2, has_expired: 3, earned_badge: 4, post_derived_from: 5 }
 end
