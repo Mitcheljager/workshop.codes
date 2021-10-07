17fe35ce6f4c76554d117d205fd9dbacaa258632
CactusPuppy <puppycactus@gmail.com>
2021-10-07 11:25:42 -0700

    Allow deterministic seeding of RSpec and Cucumber - This allows intermittent test case failures due to randomization to be reproduced reliably - To set a seed in Cucumber, use `cucumber SEED=<seed>`

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -1,7 +1,20 @@
 require 'factory_bot'
+require 'cucumber'
 
 # Enable use of factory methods without needing to prefix FactoryBot in step definitions
 World(FactoryBot::Syntax::Methods)
 
 
 Capybara.javascript_driver = :selenium_headless
+
+# Set the global random seed for reproducible test runs
+AfterConfiguration do |config|
+  $seed = ENV['SEED'].present? ? ENV['SEED'].to_i : config.seed
+  Kernel.srand $seed
+  puts "Setting global random seed to #{ $seed }"
+end
+
+# Report the seed after run
+at_exit do
+  puts "Global random seed was #{ $seed }"
+end
