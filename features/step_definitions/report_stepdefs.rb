5ce8ec635b32ed4424ea983b7921767654feb05d
CactusPuppy <puppycactus@gmail.com>
2021-10-07 11:27:49 -0700

    Fix issue where filling in report form could fail due to selecting the default option instead of a valid option

--- features/step_definitions/report_stepdefs.rb
+++ features/step_definitions/report_stepdefs.rb
@@ -53,6 +53,6 @@ end
 
 def fill_in_report_form
   report_attrs = attributes_for(:report)
-  find("#report_category").all('option').sample.select_option
+  find("#report_category").all('option').filter{ |e| e.value.present? }.sample.select_option
   fill_in "report_content", with: report_attrs[:content]
 end
