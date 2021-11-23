f8e5941207e07b372ec74848aa35a8915e404d41
CactusPuppy <puppycactus@gmail.com>
2021-11-22 16:09:22 -0800

    feat: Update flash display
    Added ability to show multiple flash messages and
    dismiss flash messages

---
+++ app/javascript/src/dismiss-parent.js
@@ -0,0 +1,20 @@
+export function destroy() {
+  const elements = document.querySelectorAll("[data-role~='dismiss-parent']")
+
+  elements.forEach((element) => element.parentNode.remove())
+}
+
+export function bind() {
+  const elements = document.querySelectorAll("[data-role~='dismiss-parent']")
+
+  elements.forEach((element) => element.removeAndAddEventListener("click", dismissParent))
+}
+
+function dismissParent(event) {
+  const parent = this.parentNode
+  parent.classList.add("fade-out")
+  setTimeout((parent) => {
+    parent.remove()
+  }, 500, parent)
+  event.preventDefault()
+}
