204c0a73b2de389cac0c62518e3bf264f6c8fa9d
CactusPuppy <puppycactus@gmail.com>
2021-12-14 12:53:18 -0800

    test: Add ability to check notifications
    Also makes Capybara wait for notification-specific content to appear

--- features/step_definitions/global_stepdefs.rb
+++ features/step_definitions/global_stepdefs.rb
@@ -1,3 +1,7 @@
 Then "I should see {string}" do |string|
   expect(page).to have_content(string)
 end
+
+Then "I should see a notification saying {string}" do |string|
+  page.find(".alerts__alert", text: string)
+end
