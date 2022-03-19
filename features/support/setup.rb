9b22ab32cadb19dfea1733c910834e04f40cc3a2
CactusPuppy <puppycactus@gmail.com>
2022-03-18 17:46:25 -0700

    test: Avoid deprecated Cucumber setup hook

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -10,7 +10,7 @@ OmniAuth.config.test_mode = true
 Capybara.javascript_driver = :selenium_headless
 
 # Set the global random seed for reproducible test runs
-AfterConfiguration do |config|
+InstallPlugin do |config, registry|
   $seed = ENV['SEED'].present? ? ENV['SEED'].to_i : config.seed
   Kernel.srand $seed
   puts "Setting global random seed to #{ $seed }"
