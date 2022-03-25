53c9226619308ce7c7533f0fa737beab5de7efca
CactusPuppy <puppycactus@gmail.com>
2022-03-25 11:46:20 -0700

    test: Properly stub Discord OAuth information

--- features/step_definitions/authentication_stepdefs.rb
+++ features/step_definitions/authentication_stepdefs.rb
@@ -65,14 +65,26 @@ def oauth_username_to_mock_uid(provider, username)
 end
 
 def stub_oauth_flow(provider, username, &block)
-  OmniAuth.config.add_mock(provider.to_sym, {
+  base_mock = {
     uid: oauth_username_to_mock_uid(provider, username),
     provider: provider,
     info: {
       name: username,
       image: "https://ehe.gg/media/img/logos/Elo-Hell-Logo_I-C-Dark.png"
     }
-  })
+  }
+  # Discord-specific adjustments
+  # ! If needed in the future for other services,
+  # ! can refactor adjustments into a dedicated method
+  if provider == "discord"
+    base_mock[:info][:name] = username.split("#").first
+    base_mock[:extra] = {
+      raw_info: {
+        discriminator: username.split("#").last
+      }
+    }
+  end
+  OmniAuth.config.add_mock(provider.to_sym, base_mock)
   block.call
   OmniAuth.config.mock_auth[provider.to_sym] = nil
 end
