abf6abaff391bac8faf15f48fa59e8fb06feafdb
CactusPuppy <puppycactus@gmail.com>
2022-03-29 15:32:28 -0700

    fix: Prevent accidental disclosure before release date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
   def tos; end
 
   def explanation
-    redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    if ENV["APRIL_FOOLS_CHANCE"].present?
+      redirect_to "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    else
+      raise ActionController::RoutingError.new("Not Found")
+    end
   end
 end
