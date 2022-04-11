689cabec79248588559734822f59728583164c4e
CactusPuppy <puppycactus@gmail.com>
2022-04-11 15:02:29 -0700

    fix: Gracefully handle user not found error
    This would only happen if a user was deleted but another browser had a valid
    remember token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     else
       @current_user = nil
     end
