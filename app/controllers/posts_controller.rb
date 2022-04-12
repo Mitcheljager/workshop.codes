ec7571dfcd3cdb28daea21b65bd91e2d9c242deb
CactusPuppy <puppycactus@gmail.com>
2022-04-12 15:00:02 -0700

    revert: "perf: Cache `similar_to` post search"
    Caching should ideally occur in the model, not the controller
    
    This reverts commit 13767c38c55f4f9f182884071154acfbefd6b2a1.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -215,13 +215,7 @@ class PostsController < ApplicationController
 
   def similar_to
     @post = Post.find(params[:id])
-    @posts = if ENV["BONSAI_URL"] then
-        Rails.cache.fetch("#{cache_key_with_version}/similar_to", expires_in: 6.hours) do
-          Post.includes(:user).search(@post.tags.presence || @post.title, 4).records.where.not(id: @post.id).select_overview_columns.public?.limit(3)
-        end
-      else
-        Post.where.not(id: @post.id).last(3)
-      end
+    @posts = ENV["BONSAI_URL"] ? Post.includes(:user).search(@post.tags.presence || @post.title, 4).records.where.not(id: @post.id).select_overview_columns.public?.limit(3) : Post.where.not(id: @post.id).last(3)
 
     if @posts.any?
       render collection: @posts, partial: "card", as: :post
