7b813edb1443149b1c64d39c24a0da3aa4e90890
Mitchel <mitchel_jager@hotmail.com>
2022-04-15 21:43:17 +0200

    feat: Lazy load similar codes when scrolled to

--- app/javascript/src/get-partial.js
+++ app/javascript/src/get-partial.js
@@ -4,19 +4,33 @@ export function bind() {
   const elements = document.querySelectorAll("[data-action~='get-partial']")
 
   elements.forEach(element => {
-    element.removeAndAddEventListener("click", getPartial)
-
     if (element.dataset.getOnLoad == "true") getPartial(event, element)
+    if (element.dataset.lazy == "true") {
+      document.removeAndAddEventListener("scroll", () => lazyLoadGetPartial(element), { passive: true })
+      lazyLoadGetPartial(element)
+    }
+    else element.removeAndAddEventListener("click", getPartial)
   })
+}
+
+function lazyLoadGetPartial(element) {
+  if (element.dataset.initialised == "true") return
+
+  const threshold = 100
+
+  if (element.getBoundingClientRect().top - window.outerHeight > threshold) return
+
+  element.dataset.initialised = "true"
+  getPartial(event, element)
 }
 
 function getPartial(event, element) {
   event.preventDefault()
-  
+
   const _this = element || event.target
   const targetElement = document.querySelector(`[data-partial="${ _this.dataset.target }"]`)
   const url = _this.dataset.url
-  
+
   if (targetElement.dataset.loaded == "true") return
 
   new FetchRails(url).get()
--- app/javascript/src/remove-and-add-event-listener.js
+++ app/javascript/src/remove-and-add-event-listener.js
@@ -2,3 +2,8 @@ Element.prototype.removeAndAddEventListener = function(event, funct) {
   this.removeEventListener(event, funct)
   this.addEventListener(event, funct)
 }
+
+Document.prototype.removeAndAddEventListener = function(event, funct) {
+  this.removeEventListener(event, funct)
+  this.addEventListener(event, funct)
+}
