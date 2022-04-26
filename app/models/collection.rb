04a7ce5b34044b6dd012713018b6f32b8a88be27
Mitchel <mitchel_jager@hotmail.com>
2022-04-26 19:54:46 +0200

    fix: Error when viewing collections due to overwatch_2_compatible missing

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,7 @@
 class Collection < ApplicationRecord
   belongs_to :user
 
-  has_many :posts, -> { select(:id, :title, :code, :ptr, :image_order, :maps, :user_id, :categories, :updated_at, :last_revision_created_at, :immortal ) }
+  has_many :posts, -> { select(:id, :title, :code, :ptr, :overwatch_2_compatible, :image_order, :maps, :user_id, :categories, :updated_at, :last_revision_created_at, :immortal ) }
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 end
