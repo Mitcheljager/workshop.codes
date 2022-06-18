0c88cb893e65113cc618d4c31e5d8e9af6304b76
Mitchel <mitchel_jager@hotmail.com>
2022-06-18 20:04:59 +0200

    chore: Remove Google Analytics

--- app/javascript/src/analytics.js
+++
@@ -1,8 +0,0 @@
-export function send() {
-  if (typeof ga === "function") {
-    gtag("config", "UA-46852314-10", {
-      "anonymize_ip": true,
-      "page_location": event.data.url
-    })
-  }
-}
