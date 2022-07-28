a519632bd23d827a582a60f96a3fd179fdb7790d
CactusPuppy <puppycactus@gmail.com>
2022-07-27 19:02:42 -0700

    fix: Give proper response format for old filter URLs

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,7 +1,12 @@
 class FilterController < ApplicationController
   def index
     # REFACTOR: Make this set of attributes have a single source of accessible truth
-    redirect_to filter_path(params.permit([:category, :hero, :map, :from, :to, :sort, :expired, :author, :players, :language, :search, :default, :page])), status: :moved_permanently
+    filtered_params = params.permit([:category, :hero, :map, :from, :to, :sort, :expired, :author, :players, :language, :search, :default, :page])
+    respond_to do |format|
+      format.html   { redirect_to filter_path(filtered_params), status: :moved_permanently }
+      format.js     { redirect_to filter_path(filtered_params), status: :moved_permanently }
+      format.json   { redirect_to filter_path(filtered_params), status: :moved_permanently }
+    end
   end
 
   def get_verified_users
