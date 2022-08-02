e375fbd641b52c9e48d85c3f5ff0a22225a412ba
CactusPuppy <puppycactus@gmail.com>
2022-08-01 18:46:41 -0700

    fix: Don't include default language in passed parameters

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,7 +1,7 @@
 class FilterController < ApplicationController
   def index
     # REFACTOR: Make this set of attributes have a single source of accessible truth
-    filtered_params = params.permit([:category, :hero, :map, :from, :to, :sort, :expired, :author, :players, :language, :search, :default, :page])
+    filtered_params = params.permit([:category, :hero, :map, :from, :to, :sort, :expired, :author, :players, :language, :search, :page])
     respond_to do |format|
       format.html   { redirect_to filter_path(filtered_params), status: :moved_permanently }
       format.js     { redirect_to filter_path(filtered_params), status: :moved_permanently }
