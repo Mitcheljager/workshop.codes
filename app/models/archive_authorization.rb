e6373fab14e94d2db7e913ca863bac6e24b6746d
CactusPuppy <puppycactus@gmail.com>
2022-08-25 09:47:23 -0700

    chore: Add more granular validation of bnet_id

--- app/models/archive_authorization.rb
+++ app/models/archive_authorization.rb
@@ -1,3 +1,4 @@
 class ArchiveAuthorization < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
+  validates :bnet_id, presence: true, numericality: { only_integer: true, greater_than: 0 }, length: { minimum: 6 }
 end
