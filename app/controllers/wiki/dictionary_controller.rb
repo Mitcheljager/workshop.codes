95eb78bf837339f46f16ac82641484e53c6042cd
Mitchel <mitchel_jager@hotmail.com>
2022-09-20 02:34:16 +0200

    chore: Add comment

--- app/controllers/wiki/dictionary_controller.rb
+++ app/controllers/wiki/dictionary_controller.rb
@@ -10,7 +10,7 @@ class Wiki::DictionaryController < Wiki::BaseController
     @dictionary = merged_array.map { |a| a[1]["en-US"] }
 
     respond_to do |format|
-      format.html { render "wiki/dictionary/index.html.erb" }
+      format.html { render "wiki/dictionary/index.html.erb" } # Automatic path doesn't work, possibly because of no plurals
       format.json {
         set_request_headers
         render json: @dictionary.to_json, layout: false
