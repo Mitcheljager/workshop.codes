04214bd3f0dd023573e7dc386d31c66ee60881d0
Mitchel <mitchel_jager@hotmail.com>
2022-09-29 23:10:27 +0200

    chore: Add articles to sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -5,6 +5,7 @@ class SitemapsController < ApplicationController
     @wiki_articles = Wiki::Article.approved.group(:group_id).maximum(:id).values
     @wiki_articles = Wiki::Article.approved.where(id: @wiki_articles)
     @wiki_categories = Wiki::Category.all
+    @articles = Article.all
 
     respond_to do |format|
       format.xml
