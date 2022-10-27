a7038b43323a2355f27985a8885e20d6593d1ffd
Mitchel <mitchel_jager@hotmail.com>
2022-10-27 20:17:42 +0200

    chore: Add more characters to phraseIdentifier

--- app/javascript/src/lib/OWLanguageLegacy.js
+++ app/javascript/src/lib/OWLanguageLegacy.js
@@ -32,7 +32,7 @@ const octal = /^\-?0o[0-7][0-7_]*/
 const hexadecimal = /^\-?0x[\dA-Fa-f][\dA-Fa-f_]*(?:(?:\.[\dA-Fa-f][\dA-Fa-f_]*)?[Pp]\-?\d[\d_]*)?/
 const decimal = /^\-?\d[\d_]*(?:\.\d[\d_]*)?(?:[Ee]\-?\d[\d_]*)?/
 const identifier = /^\$\d+|(`?)[_A-Za-z][_A-Za-z$0-9]*\1/
-const phraseIdentifier = /.+?(?=[\(\)\{\};,.=+\-\n\[\]]|\s==)/
+const phraseIdentifier = /.+?(?=[\(\)\{\}:;,.=+&/\*\-\n\[\]]|\s==)/
 const variable = /(?<=Global.)(\w+)/
 const customKeywords = /(?<!\w)@\w+/
 
