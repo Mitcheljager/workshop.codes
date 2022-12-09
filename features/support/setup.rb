71591fab1fc8024623a27153cf180279980cee49
CactusPuppy <puppycactus@gmail.com>
2022-12-08 20:00:33 -0800

    ci(Cucumber): Fix failing Firefox dependency
    Due to Ubuntu 22.04 only having Firefox as a Snap package, and
    Snap packages being unable to read the `/tmp` directory (for some
    utterly stupid reason), Geckodriver must be configured to create
    the temporary Firefox profile file within a directory under the
    current user's home directory

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -8,6 +8,28 @@ World(FactoryBot::Syntax::Methods)
 # Enable mocking the OmniAuth flow
 OmniAuth.config.test_mode = true
 
+# HACK: EXTREMELY ugly
+# Because Snap-packaged Firefox is sandboxed and cannot see into
+# the /tmp directory, we need to make a clean tmp directory under the
+# user's home to store the profiles
+
+# Unfortunately, this means that the test runner must set the
+# environment variable `TMPDIR` to `$HOME/tmp` manually, either by
+# a bash command somewhat like the following:
+# ```
+# #!bin/bash
+# TMPDIR=$HOME/tmp bundle exec rake cucumber`
+# ```
+# or otherwise
+
+BeforeAll do
+  Dir.mkdir(File.join(Dir.home, "tmp")) unless File.exist?(File.join(Dir.home, "tmp"))
+end
+
+AfterAll do
+  FileUtils.remove_dir(File.join(Dir.home, "tmp"), true)
+end
+
 Capybara.javascript_driver = :selenium_headless
 
 # Set the global random seed for reproducible test runs
