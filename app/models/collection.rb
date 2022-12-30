1e40fa6d2c86c4d277be6b9db879c0d7fe778dae
Mitchel <mitchel_jager@hotmail.com>
2022-12-31 00:50:42 +0100

    chore: Add cover image to collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,5 +3,7 @@ class Collection < ApplicationRecord
 
   has_many :posts, -> { select(:id, :title, :code, :ptr, :overwatch_2_compatible, :image_order, :maps, :user_id, :categories, :updated_at, :last_revision_created_at, :immortal ) }
 
+  has_one_attached :cover_image, dependent: :destroy
+
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 end
