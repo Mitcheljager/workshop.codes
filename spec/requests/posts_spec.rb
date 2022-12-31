7dbdd7f3903e0de1ff71940933eedd90c75fe80b
Mitchel <mitchel_jager@hotmail.com>
2022-12-31 15:55:09 +0100

    chore: Use constant for page size

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Posts controller", type: :request do
   let!(:user) { create(:user) }
-  let!(:posts) { create_list(:post, 20, user_id: user.id) }
+  let!(:posts) { create_list(:post, PAGE_SIZE, user_id: user.id) }
   let(:post) { posts.first }
 
   describe "GET root" do
@@ -10,7 +10,7 @@ RSpec.describe "Posts controller", type: :request do
 
     it "returns posts" do
       expect(posts).not_to be_empty
-      expect(posts.size).to eq(20)
+      expect(posts.size).to eq(PAGE_SIZE)
     end
 
     it "returns status code 200" do
