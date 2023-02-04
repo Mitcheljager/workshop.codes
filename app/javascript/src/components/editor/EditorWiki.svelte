dbea9c8f6f63568c305e49fba63643546a413a8e
Mitchel <mitchel_jager@hotmail.com>
2023-02-04 18:45:17 +0100

    chore: Open links in editor wiki article in new tab

--- app/javascript/src/components/editor/EditorWiki.svelte
+++ app/javascript/src/components/editor/EditorWiki.svelte
@@ -2,12 +2,19 @@
   import EditorWikiSearch from "./EditorWikiSearch.svelte"
 
   let article
+
+  function click(event) {
+    if (!event.target.href) return
+
+    event.preventDefault()
+    window.open(event.target.href, "_blank")
+  }
 </script>
 
 <EditorWikiSearch on:select={event => article = event.detail.result} />
 
 {#if article}
-  <div class="mt-1/2">
+  <div class="mt-1/2" on:click={click}>
     <div class="text-dark mb-1/8">{article.category.title}</div>
     <h2 class="mt-0 mb-1/8">{article.title}</h2>
     {#if article.subtitle}
