ba370f5b2ccaecfdae5e86db9a828a805a38e660
CactusPuppy <puppycactus@gmail.com>
2023-03-05 21:29:15 +0000

    test: Ensure that webpack recompiles Svelte files for RSpec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require "capybara/rspec"
 require "capybara/rails"
+require File.expand_path('./support/webpack.rb', __dir__)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
