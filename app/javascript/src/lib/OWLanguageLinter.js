4f98305979f427f950ebd32b8480ec2e9f68a37f
Mitchel <mitchel_jager@hotmail.com>
2023-03-16 01:51:39 +0100

    chore: Use highest nested action for translations check

--- app/javascript/src/lib/OWLanguageLinter.js
+++ app/javascript/src/lib/OWLanguageLinter.js
@@ -216,18 +216,22 @@ function checkTranslations(content) {
   while ((match = regex.exec(content)) != null) {
     try {
       let walk = match.index
-      let closingParenCount = 0
+      let lastParenAtIndex = -1
+      let inString = false
       while(walk) {
         const char = content[walk]
-        if (char == "@" && content.slice(walk, walk + 6) == "@mixin") break
-        if (char == ";") throw new Error("Using @translate outside of an action has no effect")
-        if (char == ")") closingParenCount++
-        if (char == "(" && !closingParenCount) break
-        if (char == "(") closingParenCount--
+        if (char == "\"") inString = !inString
+        if (!inString) {
+          if ([";", "{", "}"].includes(char)) break
+          if (char == "@" && content.slice(walk, walk + 6) == "@mixin") break
+          if (char == "(") lastParenAtIndex = walk
+        }
         walk--
       }
 
-      const phrase = getPhraseFromPosition({ text: content, from: 0 }, walk - 1)
+      if (lastParenAtIndex == -1) throw new Error("Using @translate outside of an action has no effect")
+
+      const phrase = getPhraseFromPosition({ text: content, from: 0 }, lastParenAtIndex - 1)
       const acceptedPhrases = ["Create HUD Text", "Create In-World Text", "Create Progress Bar HUD Text", "Set Objective Description", "Big Message", "Small Message"]
       if (phrase?.text.includes("include")) return
       if (phrase?.text && !acceptedPhrases.includes(phrase.text)) throw new Error(`Using @translate inside of "${ phrase.text }" has no effect.`)
@@ -283,7 +287,7 @@ function findMissingSemicolons(content) {
         continue
       }
     }
-    
+
     if (content.slice(i, i+2) == "*/"){
       inComment = true
       continue
