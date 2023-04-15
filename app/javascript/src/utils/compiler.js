a33700afc95c3f1eb5bd513301def51b991193b3
Mitchel <mitchel_jager@hotmail.com>
2023-04-16 01:47:46 +0200

    fix: Require "from" after iterable name

--- app/javascript/src/utils/compiler.js
+++ app/javascript/src/utils/compiler.js
@@ -328,9 +328,10 @@ function convertTranslations(joinedItems) {
 
 function evaluateForLoops(joinedItems) {
   let match
-  const forRegex = /@for\s+\(\s*(?:(\w+)\s+)?(?:from\s+)?(\d+)\s+(?:through\s+)?(\d+)\s*\)\s*\{/g // Matches "@for [var] [from] number through number" in groups for each param
+  const forRegex = /@for\s+\(\s*((?:(\w+)\s+)?(?:from\s+))?(\d+)\s+(?:through\s+)?(\d+)\s*\)\s*\{/g // Matches "@for [var] [from] number through number" in groups for each param
   while ((match = forRegex.exec(joinedItems)) != null) {
-    const [full, variable, start, end] = match
+    console.log(match)
+    const [full, _, variable, start, end] = match
 
     const openingBracketIndex = match.index + full.length - 1
     const closingBracketIndex = getClosingBracket(joinedItems, "{", "}", openingBracketIndex - 1)
