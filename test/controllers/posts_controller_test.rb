bf0796ca42fb4c046a7cdf282a618e5274b83e2a
CactusPuppy <puppycactus@gmail.com>
2023-06-05 18:40:26 -0700

    test: Add tests for carousel video IDs

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -27,19 +27,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
     assert page.has_content? "Create new"
 
-    code = SecureRandom.alphanumeric(5)
-
-    fill_in "post_title", with: @post.title
-    fill_in "post_code", with: SecureRandom.alphanumeric(5)
-    fill_in "post_description", with: @post.description
-    fill_in "post_tags", with: @post.tags
-    fill_in "post_version", with: @post.version
-
-    find("#post_categories").find("option[value='Team Deathmatch']").select_option
-    find("#post_heroes_reinhardt").set(true)
-    find("#post_maps_hanamura").set(true)
-    find("#post_min_players", visible: false).set(1)
-    find("#post_max_players", visible: false).set(12)
+    fill_in_post_details
 
     assert_difference "Post.count", 1 do
       click_button "Create Post"
@@ -78,4 +66,63 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
       first(".item--small").click_link "Delete"
     end
   end
+
+  test "Creating a post with an invalid carousel video ID does not create a post" do
+    sign_in_as users(:user_one)
+
+    visit new_post_url
+
+    fill_in_post_details
+
+    fill_in "post_carousel_video", with: "https://www.bilibili.com/video/BV1yw411Z7cV/"
+
+    assert_no_changes "Post.count" do
+      click_button "Create Post"
+      assert page.has_content? "Create new Workshop Code"
+    end
+  end
+
+  test "Creating a post with a YouTube video ID" do
+    sign_in_as users(:user_one)
+
+    visit new_post_url
+
+    fill_in_post_details
+
+    fill_in "post_carousel_video", with: "DAcqc5B2uEM"
+
+    click_button "Create Post"
+
+    assert_equal find(".video__iframe")["src"], "https://www.youtube-nocookie.com/embed/DAcqc5B2uEM?enablejsapi=1"
+  end
+
+  test "Creating a post with a YouTube link instead of a YouTube video ID still works as expected" do
+    sign_in_as users(:user_one)
+
+    visit new_post_url
+
+    fill_in_post_details
+
+    fill_in "post_carousel_video", with: "https://www.youtube.com/watch?v=DAcqc5B2uEM&t=20s"
+
+    click_button "Create Post"
+
+    assert_equal find(".video__iframe")["src"], "https://www.youtube-nocookie.com/embed/DAcqc5B2uEM?enablejsapi=1"
+  end
+end
+
+def fill_in_post_details
+  code = SecureRandom.alphanumeric(5)
+
+  fill_in "post_title", with: @post.title
+  fill_in "post_code", with: SecureRandom.alphanumeric(5)
+  fill_in "post_description", with: @post.description
+  fill_in "post_tags", with: @post.tags
+  fill_in "post_version", with: @post.version
+
+  find("#post_categories").find("option[value='Team Deathmatch']").select_option
+  find("#post_heroes_reinhardt").set(true)
+  find("#post_maps_hanamura").set(true)
+  find("#post_min_players", visible: false).set(1)
+  find("#post_max_players", visible: false).set(12)
 end
