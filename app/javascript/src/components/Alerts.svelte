46a4a79e26787b3b3b27d86b7fd177402706b8f8
Mitchel <mitchel_jager@hotmail.com>
2023-08-19 14:11:15 +0200

    feat: Add visible timer to individual alerts

--- app/javascript/src/components/Alerts.svelte
+++ app/javascript/src/components/Alerts.svelte
@@ -1,11 +1,10 @@
 <script>
+  import Alert from "./Alert.svelte"
   import { slide } from "svelte/transition"
 
   // When alerts are passed from rails they are passed as a string
   // that is an array of arrays shaped like [[type, text], [type, text]]
   export let initialAlerts = ""
-
-  const timeout = 3000
 
   let alerts = []
 
@@ -15,21 +14,17 @@
     alerts = [...alerts, { ...alert, key: Math.random() }]
   }
 
-  function close(index) {
-    alerts = alerts.filter((_, i) => i !== index)
+  function close(key) {
+    alerts = alerts.filter((a) => a.key !== key)
   }
 </script>
 
 <svelte:window on:alert={({ detail }) => add(detail)} />
 
 <div class="alerts">
-  {#each alerts as { text, type, key }, i (key)}
+  {#each alerts as { text, type, key } (key)}
     <div transition:slide={{ duration: 200 }}>
-      <div class="alert {type} static">
-        <p class="m-0">{text}</p>
-
-        <button class="button p-0 pl-1/16 pr-1/16 text-pure-white" on:click={() => close(i)}>✕</button>
-      </div>
+      <Alert {text} {type} {key} on:close={({ detail }) => close(detail)} />
     </div>
   {/each}
 </div>
