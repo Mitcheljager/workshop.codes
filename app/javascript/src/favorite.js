eb92ca55b302e268ddf8a47f5b183c3bd9ed90a6
Mitchel <mitchel_jager@hotmail.com>
2023-10-10 00:39:36 +0200

    test: Move favorite test to jest

--- app/javascript/src/favorite.js
+++ app/javascript/src/favorite.js
@@ -8,27 +8,30 @@ export function bind() {
 async function favorite(event) {
   let eventTarget = event.target
   if (eventTarget.dataset.action != "favorite") eventTarget = event.target.closest("[data-action~='favorite']")
-  if (!eventTarget) return
 
-  const postId = eventTarget.dataset.target
-  const active = eventTarget.dataset.active == "true"
+  if (eventTarget) toggleFavorite(eventTarget)
+}
+
+export function toggleFavorite(element) {
+  const postId = element.dataset.target
+  const active = element.dataset.active == "true"
   const body = { favorite: { post_id: postId } }
-  const imageSrc = active ? eventTarget.dataset.inactiveIcon : eventTarget.dataset.activeIcon
-  const imageElement = eventTarget.querySelector("img")
+  const imageSrc = active ? element.dataset.inactiveIcon : element.dataset.activeIcon
+  const imageElement = element.querySelector("img")
 
   imageElement.src = imageSrc
-  eventTarget.dataset.active = !active
-  eventTarget.classList.toggle("favorite--is-active", !active)
-  eventTarget.classList.toggle("favorite--animating", !active)
+  element.dataset.active = !active
+  element.classList.toggle("favorite--is-active", !active)
+  element.classList.toggle("favorite--animating", !active)
 
   new FetchRails("/favorites", body).post({ method: active ? "delete" : "post" })
     .then(() => {
       if (!active) window.dispatchEvent(new CustomEvent("favorite"))
     })
     .catch(() => {
-      imageElement.src = !active ? eventTarget.dataset.inactiveIcon : eventTarget.dataset.activeIcon
-      eventTarget.classList.toggle("favorite--is-active", active)
-      eventTarget.dataset.active = active
+      imageElement.src = !active ? element.dataset.inactiveIcon : element.dataset.activeIcon
+      element.classList.toggle("favorite--is-active", active)
+      element.dataset.active = active
 
       addAlertError("Something went wrong, please try again")
     })
