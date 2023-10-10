e7dc6d62da748339319c4978f8fc495740fabdac
Mitchel <mitchel_jager@hotmail.com>
2023-10-10 13:44:23 +0200

    feat: Add additional ollie

--- app/javascript/src/components/Ollie.svelte
+++ app/javascript/src/components/Ollie.svelte
@@ -2,15 +2,20 @@
   import { Confetti } from "svelte-confetti"
 
   export let chance = 10
-  export let image = ""
+  export let imageFlyBy = ""
+  export let imageHeart = ""
 
   let active = false
+  let type = 1
 
   function maybeShow() {
     if (active) return
     const random = Math.random() * 100
 
     if (random > chance) return
+
+    // Select the type of animation, 1 or 2
+    type = Math.ceil(Math.random() * 2)
 
     active = true
     setTimeout(() => active = false, 2000)
@@ -22,9 +27,19 @@
 <div>
   {#if active}
     <div class="ollie-overlay">
-      <img src={image} alt="" width="200" />
+      {#if type === 1}
+        <div class="ollie-fly-by">
+          <img src={imageFlyBy} alt="" width="200" />
 
-      <Confetti x={[5, 0]} y={[5, 0]} amount="100" size="15" cone colorArray={["#f06414"]} />
+          <Confetti x={[5, 0]} y={[5, 0]} amount="100" size="15" cone colorArray={["#f06414"]} />
+        </div>
+      {/if}
+
+      {#if type === 2}
+        <div class="ollie-heart">
+          <img src={imageHeart} alt="" width="200" />
+        </div>
+      {/if}
     </div>
   {/if}
 </div>
