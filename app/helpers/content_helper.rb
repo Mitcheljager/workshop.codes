74204f013e6c7e8fa203ad703c399863ab6845d8
Mitchel <mitchel_jager@hotmail.com>
2023-10-13 23:18:59 +0200

    fix: temporarily accept all strings

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -189,12 +189,14 @@ module ContentHelper
 
   def hero_name_to_icon_url(hero, size = 50)
     string = "heroes/#{ size }/#{ hero.downcase.gsub(":", "").gsub(" ", "").gsub(".", "").gsub("ú", "u").gsub("ö", "o") }.png"
-    asset_exists?(string) ? string : nil
+    # asset_exists?(string) ? string : nil
+    string
   end
 
   def ability_name_to_icon_url(ability, size = 50)
     string = "abilities/#{ size }/#{ ability.downcase.gsub(":", "").gsub(" ", "-").gsub("!", "") }.png"
-    asset_exists?(string) ? string : nil
+    # asset_exists?(string) ? string : nil
+    string
   end
 
   def sanitized_markdown(text, rendererOptions: {})
