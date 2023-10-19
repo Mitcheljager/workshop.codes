f1ad7657e993a2967fa9e3652e486572ce9baf6c
Mitchel <mitchel_jager@hotmail.com>
2023-10-19 14:47:02 +0200

    feat: Add content slots to mixins

--- app/javascript/src/utils/compiler/mixins.js
+++ app/javascript/src/utils/compiler/mixins.js
@@ -45,7 +45,7 @@ export function extractAndInsertMixins(joinedItems) {
       content: mixin,
       full: joinedItems.slice(match.index, closing + 1),
       params: paramsDefaults,
-      hasContents: mixin.includes("@contents;")
+      hasContents: mixin.includes("@contents")
     }
   }
 
@@ -57,9 +57,7 @@ export function extractAndInsertMixins(joinedItems) {
     // Get arguments
     const index = joinedItems.indexOf("@include")
     let closing = getClosingBracket(joinedItems, "(", ")", index + 1)
-    if (closing < 0) {
-      closing = joinedItems.length
-    }
+    if (closing < 0) closing = joinedItems.length
     const full = joinedItems.slice(index, closing + 1)
     const name = full.match(/(?<=@include\s)(\w+)/)?.[0]
     const mixin = mixins[name]
@@ -80,20 +78,7 @@ export function extractAndInsertMixins(joinedItems) {
     // Get content for @contents
     let fullMixin
     let contents
-    if (mixin.hasContents) {
-      let contentsClosing = getClosingBracket(joinedItems, "{", "}", index)
-      if (contentsClosing == -1) contentsClosing = joinedItems.length
-      fullMixin = joinedItems.slice(index, contentsClosing + 1)
-
-      const contentsOpening = fullMixin.indexOf("{")
-
-      if (contentsOpening != -1) {
-        contents = fullMixin.slice(contentsOpening + 1, fullMixin.length - 1)
-        if (contents.includes(`@include\s${ name }`)) throw new Error("Can not include a mixin in itself")
-      }
-
-      replaceWith = replaceWith.replace("@contents;", contents || "")
-    }
+    if (mixin.hasContents) ({ replaceWith, fullMixin, contents } = replaceContents(mixin, joinedItems, index, replaceWith))
 
     mixin.params
       .map((param, index) => ({ ...param, index }))
@@ -108,4 +93,62 @@ export function extractAndInsertMixins(joinedItems) {
   }
 
   return joinedItems
+}
+
+/**
+ * Replace every contents occurance with their corresponding slot from the mixin include.
+ */
+function replaceContents(mixin, joinedItems, index, replaceWith) {
+  let contents = ""
+  let contentsClosing = getClosingBracket(joinedItems, "{", "}", index)
+  if (contentsClosing == -1) contentsClosing = joinedItems.length
+
+  const fullMixin = joinedItems.slice(index, contentsClosing + 1)
+  const contentsOpening = fullMixin.indexOf("{")
+
+  if (contentsOpening != -1) {
+    contents = fullMixin.slice(contentsOpening + 1, fullMixin.length - 1)
+    if (contents.includes(`@include\s${ name }`)) throw new Error("Can not include a mixin in itself")
+  }
+
+  const slotContents = getSlotContents(contents, mixin)
+
+  const contentsRegex = /@contents(?:\("(.+?)"\))?;?/g
+  let match
+  while ((match = contentsRegex.exec(replaceWith)) !== null) {
+    const slot = match[1] || "default"
+    const start = match.index
+    const end = match.index + match[0].length
+
+    if (!slot in slotContents) {
+      throw new Error(`Slot "${ slot }" not found in mixin "${ mixin.name }"`)
+    }
+
+    replaceWith = replaceBetween(replaceWith, slotContents[slot], start, end)
+  }
+
+  return { contents, fullMixin, replaceWith }
+}
+
+function getSlotContents(contents, mixin) {
+  const slotContents = {}
+  const defaultSlotContent = []
+
+  const slotsRegex = /@slot\("([^"]+)"\) {/g
+  let lastIndex = 0
+  let match
+
+  while ((match = slotsRegex.exec(contents)) !== null) {
+    const name = match[1] || ""
+    const slotClosing = getClosingBracket(contents, "{", "}", match.index)
+
+    defaultSlotContent.push(contents.slice(lastIndex, match.index))
+    slotContents[name] = contents.slice(match.index + match[0].length, slotClosing)
+    lastIndex = slotClosing + 1
+  }
+
+  // Add content after final slot to default slot
+  defaultSlotContent.push(contents.slice(lastIndex))
+
+  return { ...slotContents, default: defaultSlotContent.join("") }
 }
