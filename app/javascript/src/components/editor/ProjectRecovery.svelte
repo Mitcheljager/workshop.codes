826cd63819af5739f7d8d029696060e1c9ef5792
Mitchel <mitchel_jager@hotmail.com>
2023-10-27 01:35:33 +0200

    feat: Add full date

--- app/javascript/src/components/editor/ProjectRecovery.svelte
+++ app/javascript/src/components/editor/ProjectRecovery.svelte
@@ -13,14 +13,26 @@
 
     $recoveredProject = null
   }
+
+  function toDate(date) {
+    return new Date(date)
+  }
 </script>
 
 <div class="project-recovery" transition:fly={{ y: 20, duration: 250 }}>
   <p class="mt-0 text-orange"><strong>While loading this project we found a copy on your machine that is more recent than the copy we fetched.</strong></p>
   <p>Please inspect your project closely and determine if you have lost data since you last worked on it.</p>
   <p>
-    The <strong>fetched</strong> project was last saved <em>{ timeago.format($currentProject?.updated_at || "") }</em><br>
-    The <strong>local</strong> project was last saved <em>{ timeago.format($recoveredProject?.updated_at || "") }</em>
+    The <strong>fetched</strong> project was last saved
+    <em>
+      { timeago.format($currentProject?.updated_at || "") } <br>
+      <small class="text-dark">{ toDate($currentProject?.updated_at) }</small>
+    </em> <br>
+    The <strong>local</strong> project was last saved
+    <em>
+      { timeago.format($recoveredProject?.updated_at || "") } <br>
+      <small class="text-dark">{ toDate($recoveredProject?.updated_at) }</small>
+    </em>
   </p>
 
   <button class="button bg-orange button--square" on:click={recover}>Recover</button>
