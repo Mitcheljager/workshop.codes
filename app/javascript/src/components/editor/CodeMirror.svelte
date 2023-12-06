50fd27e7e4a00bb6dc41124a2570344a9d020db2
Mitchel <mitchel_jager@hotmail.com>
2023-12-06 23:20:53 +0100

    feat(editor): Add code folding

--- app/javascript/src/components/editor/CodeMirror.svelte
+++ app/javascript/src/components/editor/CodeMirror.svelte
@@ -12,6 +12,7 @@
   import { OWLanguageLinter } from "../../lib/OWLanguageLinter"
   import { parameterTooltip } from "../../lib/parameterTooltip"
   import { extraCompletions } from "../../lib/extraCompletions"
+  import { foldBrackets } from "../../lib/foldBrackets"
   import { currentItem, editorStates, editorScrollPositions, items, currentProjectUUID, completionsMap, variablesMap, subroutinesMap, mixinsMap, settings } from "../../stores/editor"
   import { translationsMap } from "../../stores/translationKeys"
   import { getPhraseFromPosition } from "../../utils/parse"
@@ -101,6 +102,7 @@
         parameterTooltip(),
         indentationMarkers(),
         rememberScrollPosition(),
+        foldBrackets(),
         ...($settings["word-wrap"] ? [EditorView.lineWrapping] : [])
       ]
     })
---
+++ app/javascript/src/lib/foldBrackets.js
@@ -0,0 +1,21 @@
+import { foldService } from "@codemirror/language"
+import { getClosingBracket } from "../utils/parse"
+
+/**
+ * Returns fold out for curly brackets. Returns the values between the brackets, leaving the brackets in place.
+ */
+export function foldBrackets() {
+  return foldService.of((state, from) => {
+    const foldStart = state.doc.lineAt(from)
+    const startBracket = foldStart.text.search("{")
+
+    if (startBracket === -1) return
+
+    const matchingBracketIndex = getClosingBracket(state.doc.toString(), "{", "}", from)
+    const foldEnd = state.doc.lineAt(matchingBracketIndex)
+
+    if (foldStart.number === foldEnd.number) return
+
+    return { from: foldStart.from + startBracket + 1, to: matchingBracketIndex }
+  })
+}
