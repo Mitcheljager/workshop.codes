851cf2198e698e356cd345f01b674f54312de928
Mitchel <mitchel_jager@hotmail.com>
2024-02-06 12:19:46 +0100

    chore: Add mixin declaration completion

--- app/javascript/src/lib/extraCompletions.js
+++ app/javascript/src/lib/extraCompletions.js
@@ -7,6 +7,7 @@ export const extraCompletions = [
   { detail: "Editor Else If Statement", label: "@else if (Mixin.variable) {\n\n}", type: "variable" },
   { detail: "Editor Else Statement", label: "@else {\n\n}", type: "variable" },
   { detail: "Editor Each Statement", label: "@each (someVar in [one, two]) {\n\n}", type: "variable" },
+  { detail: "Editor Mixin", label: "@mixin mixinName(someVar) {\n\n}", type: "variable" },
   { detail: "Each Player Rule", label: templates.RuleEachPlayer, type: "variable" },
   { detail: "Global Rule", label: templates.RuleGlobal, type: "variable" },
   { detail: "Subroutine", label: templates.Subroutine, type: "variable" }
