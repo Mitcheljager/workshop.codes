bb293dd2e05afbdd053cfb2d4ded127a989d1c5d
Mitchel <mitchel_jager@hotmail.com>
2024-02-10 14:33:06 +0100

    chore: Move toggleable settings to the top of the settings dropdown

--- app/javascript/src/components/editor/Settings.svelte
+++ app/javascript/src/components/editor/Settings.svelte
@@ -61,6 +61,54 @@
 
   {#if active}
     <div transition:fly={{ duration: 150, y: 20 }} use:escapeable on:escape={() => active = false} class="dropdown__content block p-1/4" style="width: 300px">
+      <h5 class="mt-0 mb-1/8">Settings</h5>
+
+      <div class="checkbox tooltip mt-1/8">
+        <input id="show-line-indent-markers" type="checkbox" bind:checked={$settings["show-indent-markers"]} />
+        <label for="show-line-indent-markers">Show line indent markers</label>
+
+        <div class="tooltip__content bg-darker">
+          Show line markers at the expected indents at 1 tab or 4 spaces.
+        </div>
+      </div>
+
+      <div class="checkbox tooltip mt-1/8">
+        <input id="word-wrap" type="checkbox" bind:checked={$settings["word-wrap"]} />
+        <label for="word-wrap">Word wrap</label>
+
+        <div class="tooltip__content bg-darker">
+          Wrap lines that no longer fit on screen.
+        </div>
+      </div>
+
+      <div class="checkbox tooltip mt-1/8">
+        <input id="autocomplete-parameter-objects" type="checkbox" bind:checked={$settings["autocomplete-parameter-objects"]} />
+        <label for="autocomplete-parameter-objects">
+          Autocomplete using parameter objects
+        </label>
+
+        <div class="tooltip__content bg-darker">
+          Parameter objects change the format of parameters in actions and values to be more readable and less cumbersome to write. You can exclude any parameters you don't change the default off.
+        </div>
+      </div>
+
+      {#if $settings["autocomplete-parameter-objects"]}
+        <div class="form-group mt-1/8 tooltip" transition:slide|local={{ duration: 100 }}>
+          <label for="" class="text-base nowrap">Minimum parameter length</label>
+
+          <div class="flex align-center">
+            <input type="range" min=1 max=20 step=1 class="range mr-1/8" bind:value={$settings["autocomplete-min-parameter-size"]} />
+            {$settings["autocomplete-min-parameter-size"]}
+          </div>
+
+          <div class="tooltip__content bg-darker">
+            Only autocomplete when an action or value has equal or more than this value in parameters.
+          </div>
+        </div>
+      {/if}
+
+      <hr />
+
       <h5 class="mt-0 mb-1/8">Font</h5>
 
       <div class="form-group-inline">
@@ -99,50 +147,6 @@
       {/each}
 
       <hr>
-
-      <div class="checkbox tooltip tooltip--top-left mt-1/8">
-        <input id="show-line-indent-markers" type="checkbox" bind:checked={$settings["show-indent-markers"]} />
-        <label for="show-line-indent-markers">Show line indent markers</label>
-
-        <div class="tooltip__content bg-darker">
-          Show line markers at the expected indents at 1 tab or 4 spaces.
-        </div>
-      </div>
-
-      <div class="checkbox tooltip tooltip--top-left mt-1/8">
-        <input id="word-wrap" type="checkbox" bind:checked={$settings["word-wrap"]} />
-        <label for="word-wrap">Word wrap</label>
-
-        <div class="tooltip__content bg-darker">
-          Wrap lines that no longer fit on screen.
-        </div>
-      </div>
-
-      <div class="checkbox tooltip tooltip--top-left mt-1/8">
-        <input id="autocomplete-parameter-objects" type="checkbox" bind:checked={$settings["autocomplete-parameter-objects"]} />
-        <label for="autocomplete-parameter-objects">
-          Autocomplete using parameter objects
-        </label>
-
-        <div class="tooltip__content bg-darker">
-          Parameter objects change the format of parameters in actions and values to be more readable and less cumbersome to write. You can exclude any parameters you don't change the default off.
-        </div>
-      </div>
-
-      {#if $settings["autocomplete-parameter-objects"]}
-        <div class="form-group mt-1/8 tooltip tooltip--top-left" transition:slide={{ duration: 100 }}>
-          <label for="" class="text-base nowrap">Minimum parameter length</label>
-
-          <div class="flex align-center">
-            <input type="range" min=1 max=20 step=1 class="range mr-1/8" bind:value={$settings["autocomplete-min-parameter-size"]} />
-            {$settings["autocomplete-min-parameter-size"]}
-          </div>
-
-          <div class="tooltip__content bg-darker">
-            Only autocomplete when an action or value has equal or more than this value in parameters.
-          </div>
-        </div>
-      {/if}
 
       <button class="button button--link button--small pb-0 mt-1/4" on:click={resetToDefault}>Reset all to default</button>
     </div>
