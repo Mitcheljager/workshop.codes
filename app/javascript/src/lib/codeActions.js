a57f6984507bae05895f56c8b27e9083810df6f3
netux <martin.rodriguez+github-com@netux.site>
2024-03-24 22:55:21 -0300

    feat(editor): implement codeActions extension
    The extension leverages CodeMirror decorators, adding a lightbulb to the editor with options the user can choose to refactor a piece of their code.
    
    Actions are generated via CodeActionProviders, which are custom functions that return a list of actions with a position (for the lightbulb), a label, and a run function.
    The code includes an example code action provider which checks if the cursor is at the beginning of the words "@codeActionTest", and if so provides an "It works?" action.

---
+++ app/javascript/src/lib/codeActions.js
@@ -0,0 +1,96 @@
+import { Decoration, ViewPlugin, ViewUpdate, WidgetType } from "@codemirror/view"
+
+/**
+ * @typedef CodeAction
+ * @property {number} position
+ * @property {string} label
+ * @property {Function} run
+ */
+
+/**
+ * @typedef {(update: ViewUpdate) => CodeAction[]} CodeActionProvider
+ */
+
+class CodeActionLightBulbWidget extends WidgetType {
+  constructor(actions) {
+    super()
+
+    this.actions = actions
+  }
+
+  toDOM() {
+    const container = document.createElement("span")
+    container.className = "button cm-codeActionsLightbulb"
+    container.textContent = "💡"
+
+    const dropdown = document.createElement("select")
+    dropdown.className = "cm-codeActionsLightbulb__dropdown"
+    container.appendChild(dropdown)
+
+    const blankOption = document.createElement("option")
+    blankOption.style.display = "none"
+    dropdown.appendChild(blankOption)
+
+    for (let actionIndex = 0; actionIndex < this.actions.length; actionIndex++) {
+      const action = this.actions[actionIndex]
+
+      const option = document.createElement("option")
+      option.value = actionIndex.toString()
+      option.text = action.label
+      dropdown.appendChild(option)
+    }
+
+    container.addEventListener("click", () => dropdown.click())
+    dropdown.addEventListener("change", (event) => {
+      const actionIndex = parseInt(event.target.value, 10)
+      const action = this.actions[actionIndex]
+
+      action.run()
+    })
+
+    return container
+  }
+}
+
+function createLightBulbDecoration(position, actions) {
+  return Decoration.widget({
+    widget: new CodeActionLightBulbWidget(actions),
+    side: -1, // -1 for left, 1 for right
+    block: false
+  }).range(position)
+}
+
+/**
+ * @param {CodeActionProvider[]} providers
+ */
+export function codeActions(providers) {
+  return ViewPlugin.fromClass(class {
+    constructor(_view) {
+      this.decorations = Decoration.none
+    }
+
+    /**
+     * @param {ViewUpdate} update
+     */
+    update(update) {
+      if (update.docChanged || update.selectionSet || update.viewportChanged) {
+        this.decorations = Decoration.none
+
+        const actions = providers
+          .map((provider) => provider(update) ?? [])
+          .flat()
+
+        if (actions.length > 0) {
+          const { from } = update.state.selection.main
+          const decorationPosition = actions.reduce((position, action) => Math.min(action.position ?? Infinity, position), from)
+
+          this.decorations = Decoration.set([
+            createLightBulbDecoration(decorationPosition, actions)
+          ])
+        }
+      }
+    }
+  }, {
+    decorations: (extension) => extension.decorations
+  })
+}
