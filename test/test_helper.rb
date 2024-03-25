5463a9b163beb8b37c77ccd8cf2ea94ddee07d9e
Mitchel <mitchel_jager@hotmail.com>
2024-03-25 22:56:30 +0100

    chore: Remove more locale references

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,9 +29,3 @@ end
 class ActionDispatch::IntegrationTest
   include SignInHelper
 end
-
-class ActionDispatch::Routing::RouteSet
-  def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
-  end
-end
