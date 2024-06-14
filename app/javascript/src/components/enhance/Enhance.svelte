f1de773f92456a91339ddc9ee20cb4e1e374f005
Mitchel <mitchel_jager@hotmail.com>
2024-06-14 22:48:02 +0200

    refactor: Use guard clauses

--- app/javascript/src/components/enhance/Enhance.svelte
+++ app/javascript/src/components/enhance/Enhance.svelte
@@ -27,15 +27,15 @@
       let mostRecentDate = new Date(0)
 
       for await (const entry of directoryHandle.values()) {
-        if (entry.kind === "file") {
-          const fileHandle = await directoryHandle.getFileHandle(entry.name)
-          const file = await fileHandle.getFile()
+        if (entry.kind != "file") continue
 
-          if (file.lastModifiedDate > mostRecentDate) {
-            mostRecentDate = file.lastModifiedDate
-            mostRecentFile = file
-          }
-        }
+        const fileHandle = await directoryHandle.getFileHandle(entry.name)
+        const file = await fileHandle.getFile()
+
+        if (file.lastModifiedDate < mostRecentDate) continue
+
+        mostRecentDate = file.lastModifiedDate
+        mostRecentFile = file
       }
 
       if (!mostRecentFile) return
