7891f74d3b7b93abc8c01a998c8a6c4dfb907277
Mitchel <mitchel_jager@hotmail.com>
2024-06-17 22:34:26 +0200

    feat: Generate custom og_image for posts without thumbnail

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ group :production do
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "grover", "~> 1.1"
---
+++ config/initializers/grover.rb
@@ -0,0 +1,8 @@
+Grover.configure do |config|
+  config.options = {
+    viewport: {
+      width: 900,
+      height: 500
+    },
+  }
+end
