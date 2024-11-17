a6653365593fbbc68ee4e2753927874d47f7dcc9
Mitchel <mitchel_jager@hotmail.com>
2024-11-17 02:39:05 +0100

    refactor: Use 	his directly

--- app/javascript/src/debounce.ts
+++ app/javascript/src/debounce.ts
@@ -2,11 +2,9 @@ export default function debounce<T extends(...args: any[]) => any>(fun: T, wait 
   let timeout: ReturnType<typeof setTimeout> | null
 
   return function(this: ThisParameterType<T>, ...args: Parameters<T>): void {
-    const context = this
-
     const later = () => {
       timeout = null
-      if (!immediate) fun.apply(context, args)
+      if (!immediate) fun.apply(this, args)
     }
 
     const callNow = immediate && !timeout
@@ -14,6 +12,6 @@ export default function debounce<T extends(...args: any[]) => any>(fun: T, wait 
     if (timeout) clearTimeout(timeout)
     timeout = setTimeout(later, wait)
 
-    if (callNow) fun.apply(context, args)
+    if (callNow) fun.apply(this, args)
   }
 }
