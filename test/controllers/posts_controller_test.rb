fdfb9287a0f8c4613307115901fd2799c9ac398c
Mitchel <mitchel_jager@hotmail.com>
2024-11-18 17:13:54 +0100

    test: Adjust post index tests
    Why now? This has been unchanged for years

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -13,7 +13,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     visit posts_url
 
-    assert page.has_content? "On Fire Codes"
+    assert page.has_content? "On Fire Workshop Codes"
     assert page.has_content? "Latest Codes"
 
     assert page.has_content? posts(:post_one).title
