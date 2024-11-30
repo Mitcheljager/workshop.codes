bd73952bf1a8b3bc1dabb300d29fe262aad21723
Mitchel <mitchel_jager@hotmail.com>
2024-11-30 22:28:40 +0100

    feat: Add number input to range input as secondary input

--- app/javascript/src/components/editor/CustomGameSettings/SettingsRange.svelte
+++ app/javascript/src/components/editor/CustomGameSettings/SettingsRange.svelte
@@ -2,10 +2,11 @@
   export let item
   export let label
 
-  let value = item.values.default
+  let value = item.default
 </script>
 
 <div class="form-group mt-0">
   <label class="form-label" for="">{label}</label>
-  <input class="range" type="range" min={item.values.min} max={item.values.max} bind:value />
+  <input class="range" type="range" min={item.min} max={item.max} step="5" bind:value />
+  <input class="inline-input" style="max-width: 3rem" type="number" bind:value />
 </div>
