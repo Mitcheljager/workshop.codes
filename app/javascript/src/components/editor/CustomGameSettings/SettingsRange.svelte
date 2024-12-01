bc52edc943ac9f709a09f658abf0b5eb27b61564
Mitchel <mitchel_jager@hotmail.com>
2024-12-01 14:43:13 +0100

    feat: Allow precise values in setting range

--- app/javascript/src/components/editor/CustomGameSettings/SettingsRange.svelte
+++ app/javascript/src/components/editor/CustomGameSettings/SettingsRange.svelte
@@ -1,20 +1,24 @@
 <script>
   import { createEventDispatcher } from "svelte"
 
-  const dispatch = createEventDispatcher()
-
   export let item
   export let label
+
+  const dispatch = createEventDispatcher()
+
+  const { min, max, multiplier, precise } = item
+  const step = precise ? 0.1 : max > 50 ? 5 : 1
 
   let value = item.default
 
   function change() {
-    dispatch("change", parseInt(value))
+    const parsed = precise ? parseFloat(value) : parseInt(value) * (multiplier || 1)
+    dispatch("change", parsed)
   }
 </script>
 
 <div class="form-group mt-0">
   <label class="form-label mb-0" for="">{label}</label>
-  <input class="range" type="range" min={item.min} max={item.max} step={item.max > 50 ? 5 : 1} bind:value on:change={change} />
+  <input class="range" type="range" {min} {max} {step} bind:value on:change={change} />
   <input class="inline-input" style="max-width: 3rem" type="number" bind:value on:change={change} />
 </div>
