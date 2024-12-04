81e3a7047d960c6512dd869f09cc35b16de99ecd
Mitchel <mitchel_jager@hotmail.com>
2024-12-04 18:49:44 +0100

    feat: Add function to insert given values into custom game settings object

--- app/javascript/src/utils/customGameSettings.ts
+++ app/javascript/src/utils/customGameSettings.ts
@@ -1,10 +1,8 @@
 import { get } from "svelte/store"
 import { customGameSettings, heroes } from "@stores/editor"
 
-/**
- * Construct settings object from the customGameSettings store. Most options are one to one with the store, but some keys are build out from other values.
- * For instance, hero settings have both individual settings are settings that are inserted per hero.
- */
+/** Construct settings object from the customGameSettings store. Most options are one to one with the store, but some keys are build out from other values.
+ * For instance, hero settings have both individual settings are settings that are inserted per hero. */
 export function constructCustomGameSettings(): object {
   const settings: any = { ...get(customGameSettings) }
   const settingsHeroes = settings.heroes?.values || []
@@ -25,9 +23,7 @@ export function constructCustomGameSettings(): object {
   return settings
 }
 
-/**
- * Parse a Workshop settings string to a javascript object.
- */
+/** Parse a Workshop settings string to a javascript object.*/
 export function parseCustomGameSettingsStringToObject(input: string): object {
   input = input.replaceAll(/\s*{/g, " {") // Move opening { to the same line if it's on the next line
 
@@ -62,4 +58,52 @@ export function parseCustomGameSettingsStringToObject(input: string): object {
   }
 
   return result
+}
+
+/** Transform a settings object as returned from `parseCustomGameSettingsString` to that as returned from `constructCustomGameSettings` */
+export function settingsObjectToCustomGameSettingsFormat(input: any): object {
+  // This is set to { values: {} } rather than top level to keep subsequent checks simpler. Iterations check for the
+  // values key and by including it here we don't need to make an exception just for this top level value.
+  const settings = { values: constructCustomGameSettings() }
+  const keyChain: string[] = ["settings"]
+
+  let currentObject: any = {}
+
+  // Iterate over all keys in settings, ignore "values", and match keys in input.
+  // All keys are iterated over even if they are not present in the input.
+  // This is a but wasteful, but doing it the other way around broke my brain.
+  iterate(settings)
+
+  function iterate(item: any): void {
+    // If a key is not an object it is a value set in the settings, for instance "Ultimate Generation: 100%"
+    // This value is inserted as the `current` value, which is used by various inputs.
+    if (typeof item.values !== "object" || Array.isArray(item.values)) {
+      let valueInInput = keyChain.reduce((current, key) => current && current[key], input)
+      if (Array.isArray(item.values)) valueInInput =  Object.keys(valueInInput)
+
+      // If a value is in the settings object but not in the input it is returned as `undefined`
+      // This means no value is given and it can be ignored.
+      if (typeof valueInInput != "undefined") currentObject.current = valueInInput
+
+      keyChain.pop()
+      return
+    }
+
+    Object.entries(item.values).forEach(([key, value]) => {
+      if (!key || !value) return
+      if (key !== "values" && typeof value === "object" && !Array.isArray(value)) {
+        // A given value is an object and not a direct setting.
+        // We go 1 level deeper into the object and save the current value to be used when setting the value.
+        keyChain.push(key)
+        currentObject = value
+      }
+
+      iterate(value)
+    })
+
+    // We've gone through all keys in an object and can pop the last key in the chain before moving on to the next object
+    keyChain.pop()
+  }
+
+  return settings.values
 }
--- app/javascript/test/utils/customGameSettings.test.js
+++ app/javascript/test/utils/customGameSettings.test.js
@@ -1,5 +1,6 @@
-import { parseCustomGameSettingsStringToObject } from "@utils/customGameSettings"
-import { describe, it, expect } from "vitest"
+import { parseCustomGameSettingsStringToObject, settingsObjectToCustomGameSettingsFormat } from "@utils/customGameSettings"
+import { customGameSettings } from "@stores/editor"
+import { describe, it, expect, beforeEach } from "vitest"
 
 describe("customGameSettings.js", () => {
   describe("parseCustomGameSettingsStringToObject", () => {
@@ -27,16 +28,18 @@ describe("customGameSettings.js", () => {
           }
         }
       `)).toEqual({
-        main: {
-          "Some Key": "Some Value",
-          "Some Other Key": "Some Other Value"
-        },
-        modes: {
-          General: {
-            "Some Third Key": "Some Third Value"
+        settings: {
+          main: {
+            "Some Key": "Some Value",
+            "Some Other Key": "Some Other Value"
           },
-          Other: {
-            "Some Fourth Key": "Some Fourth Value"
+          modes: {
+            General: {
+              "Some Third Key": "Some Third Value"
+            },
+            Other: {
+              "Some Fourth Key": "Some Fourth Value"
+            }
           }
         }
       })
@@ -66,6 +69,107 @@ describe("customGameSettings.js", () => {
             Deathmatch: {},
             "Team Deathmatch": {
               "Some Key": "Some Value"
+            }
+          }
+        }
+      })
+    })
+  })
+
+  describe("settingsObjectToCustomGameSettingsFormat", () => {
+    beforeEach(() => {
+      customGameSettings.set({
+        lobby: {
+          values: {
+            General: {
+              values: {
+                "Some Key With Array": {
+                  default: "Value One",
+                  values: [
+                    "Value One",
+                    "Value Two",
+                    "Value Three"
+                  ]
+                },
+                "Some Range": {
+                  values: "range",
+                  min: 0,
+                  max: 10,
+                  default: 5
+                },
+                "Some String": {
+                  values: "string",
+                  default: "Hey"
+                }
+              }
+            },
+            "Not General": {
+              values: {
+                "Some String": {
+                  default: "Some default value"
+                }
+              }
+            }
+          }
+        }
+      })
+    })
+
+    it("Should add values given in object to return value", () => {
+      const input = {
+        settings: {
+          lobby: {
+            General: {
+              "Some Key With Array": {
+                "First Value": "",
+                "Second Value": ""
+              },
+              "Some Range": 2
+            },
+            "Not General": {
+              "Some String": "Some Value"
+            }
+          }
+        }
+      }
+
+      expect(settingsObjectToCustomGameSettingsFormat(input)).toEqual({
+        lobby: {
+          values: {
+            General: {
+              values: {
+                "Some Key With Array": {
+                  current: [
+                    "First Value",
+                    "Second Value"
+                  ],
+                  default: "Value One",
+                  values: [
+                    "Value One",
+                    "Value Two",
+                    "Value Three"
+                  ]
+                },
+                "Some Range": {
+                  values: "range",
+                  min: 0,
+                  max: 10,
+                  default: 5,
+                  current: 2
+                },
+                "Some String": {
+                  values: "string",
+                  default: "Hey"
+                }
+              }
+            },
+            "Not General": {
+              values: {
+                "Some String": {
+                  default: "Some default value",
+                  current: "Some Value"
+                }
+              }
             }
           }
         }
