e294c1e61d640afcf80c868a6264fa246d9482b7
Mitchel <mitchel_jager@hotmail.com>
2025-02-08 21:16:55 +0100

    chore: Filter out email in bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,5 +3,5 @@ return unless Rails.env.production?
 Bugsnag.configure do |config|
   config.api_key = ENV["BUGSNAG_API_KEY"]
   config.app_version = ENV["HEROKU_RELEASE_VERSION"]
-  config.meta_data_filters += ["ip", "ip_address", "clientIp", "client_ip", "remote_ip", "id", "X-Forwarded-For"]
+  config.meta_data_filters += ["ip", "ip_address", "clientIp", "client_ip", "remote_ip", "id", "X-Forwarded-For", /^email$/]
 end
