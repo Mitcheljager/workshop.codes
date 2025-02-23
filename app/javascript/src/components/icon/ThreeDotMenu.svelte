fbdfbdb9627d4c59b105ed9a8b04e232a372cc14
Mitchel <mitchel_jager@hotmail.com>
2025-02-23 18:07:13 +0100

    refactor(svelte5): Convert kebab icon to ts and Svelte 5

--- app/javascript/src/components/icon/ThreeDotMenu.svelte
+++ app/javascript/src/components/icon/ThreeDotMenu.svelte
@@ -1,21 +1,20 @@
-<script>
-  export let horizontal = false
+<script lang="ts">
+  interface Props {
+    horizontal?: boolean
+  }
+
+  const { horizontal = false }: Props = $props()
 </script>
 
 <svg
   width={horizontal ? 24 : 12}
   height={horizontal ? 12 : 24}
-  viewBox={horizontal ? "0 0 100 50" : "0 0 50 100"}
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:svg="http://www.w3.org/2000/svg"
->
+  viewBox={horizontal ? "0 0 100 50" : "0 0 50 100"}>
   {#each [20, 50, 80] as c}
     <circle
-      style="fill:currentColor;stroke:none;"
+      style="fill:currentColor; stroke:none;"
       cx={horizontal ? c : 25}
       cy={horizontal ? 25 : c}
-      r={10}
-    />
+      r={10} />
   {/each}
 </svg>
