bf2d39cf48f176d65ec07a41118f52bf1929be79
Martín (Netux) Rodríguez <send.email@netux.site>
2025-03-15 12:08:14 -0300

    refactor(editor translations modal): use Copy icon in <EditorItemDuplicate>
    Ironically, we don't want to duplicate uses of this icon.

--- app/javascript/src/components/editor/EditorItemDuplicate.svelte
+++ app/javascript/src/components/editor/EditorItemDuplicate.svelte
@@ -1,5 +1,6 @@
 <script>
   import { duplicateItem } from "@utils/editor"
+  import Copy from "@src/components/icon/Copy.svelte"
 
   export let item
 
@@ -9,5 +10,5 @@
 </script>
 
 <button on:click={duplicate} data-tooltip="Duplicate" aria-label="Duplicate">
-  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+  <Copy width="24" />
 </button>
