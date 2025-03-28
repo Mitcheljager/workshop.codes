4ead484be76851b1b6b610b2ac89223eea3f5375
Mitchel <mitchel_jager@hotmail.com>
2025-03-28 20:51:39 +0100

    fix: Move outsideclick to container

--- app/javascript/src/components/Notifications.svelte
+++ app/javascript/src/components/Notifications.svelte
@@ -58,7 +58,7 @@
   }
 </script>
 
-<div class="notifications dropdown lg-down:dropup mb-1/8 mbl:mb-0 mbl:mr-1/8">
+<div class="notifications dropdown lg-down:dropup mb-1/8 mbl:mb-0 mbl:mr-1/8" use:outsideClick={{ onOutsideClick: () => active = false }}>
   <button data-action="toggle-dropdown" aria-label="Notifications" onclick={() => toggle()}>
     <div class="notifications__bell">
       <Bell {animating} />
@@ -74,10 +74,7 @@
   </button>
 
   {#if active}
-    <div
-      class="dropdown__content dropdown__content--large active pt-0"
-      transition:fly={{ y: 10, duration: 100 }}
-      use:outsideClick={{ onOutsideClick: () => active = false }}>
+    <div class="dropdown__content dropdown__content--large active pt-0" transition:fly={{ y: 10, duration: 100 }}>
 
       <div class="dropdown__header">
         <h4 class="mt-0 mb-0">Notifications</h4>
