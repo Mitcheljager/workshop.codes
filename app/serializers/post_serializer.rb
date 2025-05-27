8864a462f3f8ff2416ce728e43801a4760aea656
Mitchel <mitchel_jager@hotmail.com>
2025-05-27 22:24:45 +0200

    feat(api): Add images array and youtube video id to api

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :code, :nice_url, :title, :thumbnail, :categories, :maps, :heroes, :tags, :created_at, :updated_at, :last_revision_created_at
+  attributes :code,
+    :nice_url, :title,
+    :thumbnail, :images, :carousel_video_youtube_id, :carousel_video_youtube_embed_url,
+    :categories, :maps, :heroes, :tags,
+    :created_at, :updated_at, :last_revision_created_at
 
   belongs_to :user
 
@@ -12,11 +16,11 @@ class PostSerializer < ActiveModel::Serializer
       image = object.images.find_by_blob_id(JSON.parse(object.image_order).first)
 
       if image
-        url = rails_public_blob_url(image.variant(quality: 95, resize_to_fill: [690, 394]).processed)
+        url = rails_public_blob_url(image.variant(quality: 95, resize_to_fill: [600, 342]).processed)
       end
     end
 
-    unless url.present?
+    if image.blank?
       random_with_seed = Random.new(object.id).rand(object.maps.length)
       maps_array = YAML.safe_load(File.read(Rails.root.join("config/arrays", "maps.yml")))
 
@@ -26,6 +30,26 @@ class PostSerializer < ActiveModel::Serializer
     end
 
     return url
+  end
+
+  def images
+    return [] unless object.image_order.present?
+
+    image_ids = JSON.parse(object.image_order)
+
+    urls = []
+
+    image_ids.each do |image_id|
+      image = object.images.find_by_blob_id(image_id)
+
+      if image
+        url = rails_public_blob_url(image.variant(quality: 95, resize_to_fill: [900, 560]).processed)
+        puts url
+        urls.push(url)
+      end
+    end
+
+    return urls
   end
 
   def maps
@@ -38,5 +62,13 @@ class PostSerializer < ActiveModel::Serializer
 
   def categories
     object.categories
+  end
+
+  def carousel_video_youtube_id
+    object.carousel_video
+  end
+
+  def carousel_video_youtube_embed_url
+    "https://www.youtube-nocookie.com/embed/" + object.carousel_video
   end
 end
