b54d9cfd4e08299ccdf35c8ed07fc54fe1d27b2b
Mitchel <mitchel_jager@hotmail.com>
2025-07-30 22:39:13 +0200

    chore: Add completions for slots

--- app/javascript/src/lib/extraCompletions.ts
+++ app/javascript/src/lib/extraCompletions.ts
@@ -9,6 +9,9 @@ export const extraCompletions: Completion[] = [
   { detail: "Editor Else Statement", label: "@else {\n\n}", type: "snippet" },
   { detail: "Editor Each Statement", label: "@each (someVar in [one, two]) {\n\n}", type: "snippet" },
   { detail: "Editor Mixin", label: "@mixin mixinName(someVar) {\n\n}", type: "snippet" },
+  { detail: "Editor Mixin Contents", label: "@contents;", type: "snippet" },
+  { detail: "Editor Mixin Named Contents", label: "@contents(\"name\");", type: "snippet" },
+  { detail: "Editor Mixin Slot", label: "@slot(\"name\"); {\n\n}", type: "snippet" },
   { detail: "Each Player Rule", label: templates.RuleEachPlayer, type: "rule" },
   { detail: "Global Rule", label: templates.RuleGlobal, type: "rule" },
   { detail: "Subroutine", label: templates.Subroutine, type: "rule" }
