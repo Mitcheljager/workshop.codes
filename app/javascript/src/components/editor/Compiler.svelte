6f3d3f3e099afd03ac2bbdff0e3b25400b3d23a7
CactusPuppy <puppycactus@gmail.com>
2025-11-20 22:32:18 -0800

    feat: compile without settings

--- app/javascript/src/components/editor/Compiler.svelte
+++ app/javascript/src/components/editor/Compiler.svelte
@@ -12,13 +12,14 @@
   let compiling = false
   let copied = false
   let dropdownActive = false
+  let compileWithSettings = true
 
   function doCompile(singleLanguageOverride = null) {
     compiling = true
     dropdownActive = false
 
     try {
-      const compiled = compile(null, singleLanguageOverride)
+      const compiled = compile(null, singleLanguageOverride, compileWithSettings)
 
       setTimeout(() => {
         compiling = false
@@ -69,27 +70,29 @@
   {/if}
 </button>
 
-{#if $selectedLanguages.length && Object.keys($translationKeys).length}
-  <div class:dropdown={!inline} use:outsideClick on:outsideClick={() => dropdownActive = false}>
-    {#if inline}
+<div class:dropdown={!inline} use:outsideClick on:outsideClick={() => dropdownActive = false}>
+  {#if !inline}
+    <button aria-label="Backups" class="button button--square button--secondary pr-0 pl-0 h-100 flex align-center" on:click={() => dropdownActive = !dropdownActive}>
+      <svg width="18px" height="18px" viewBox="0 0 24 24" class="m-0">
+        <path d="M7.00003 8.5C6.59557 8.5 6.23093 8.74364 6.07615 9.11732C5.92137 9.49099 6.00692 9.92111 6.29292 10.2071L11.2929 15.2071C11.6834 15.5976 12.3166 15.5976 12.7071 15.2071L17.7071 10.2071C17.9931 9.92111 18.0787 9.49099 17.9239 9.11732C17.7691 8.74364 17.4045 8.5 17 8.5H7.00003Z" fill="white" />
+      </svg>
+    </button>
+  {/if}
+  {#if dropdownActive || inline}
+    <div transition:fly={{ duration: 150, y: 20 }} use:escapeable on:escape={() => dropdownActive = false} class:dropdown__content={!inline} class="block w-100" style="width: 200px">
+      <div class="checkbox ml-1/4 pt-1/8 pb-1/8">
+        <input id="compile-with-settings" type="checkbox" bind:checked={compileWithSettings} />
+        <label for="compile-with-settings" class="text-small">Include Settings</label>
+      </div>
       <hr />
-    {:else}
-      <button aria-label="Backups" class="button button--square button--secondary pr-0 pl-0 h-100 flex align-center" on:click={() => dropdownActive = !dropdownActive}>
-        <svg width="18px" height="18px" viewBox="0 0 24 24" class="m-0">
-          <path d="M7.00003 8.5C6.59557 8.5 6.23093 8.74364 6.07615 9.11732C5.92137 9.49099 6.00692 9.92111 6.29292 10.2071L11.2929 15.2071C11.6834 15.5976 12.3166 15.5976 12.7071 15.2071L17.7071 10.2071C17.9931 9.92111 18.0787 9.49099 17.9239 9.11732C17.7691 8.74364 17.4045 8.5 17 8.5H7.00003Z" fill="white" />
-        </svg>
-      </button>
-    {/if}
-
-    {#if dropdownActive || inline}
-      <div transition:fly={{ duration: 150, y: 20 }} use:escapeable on:escape={() => dropdownActive = false} class:dropdown__content={!inline} class="block w-100" style="width: 200px">
+      {#if $selectedLanguages.length && Object.keys($translationKeys).length}
         <p class="pl-1/4 pr-1/4 mt-1/8 mb-1/8" class:text-small={inline}>Compile for a single language only</p>
         <p class="pl-1/4 pr-1/4 mt-0 text-small text-dark">Compile translations as standard Custom Strings for the given language.</p>
 
         {#each $selectedLanguages as language}
           <button class="dropdown__item" on:click={() => doCompile(language)}>{languageOptions[language].name}</button>
         {/each}
-      </div>
-    {/if}
-  </div>
-{/if}
+      {/if}
+    </div>
+  {/if}
+</div>
