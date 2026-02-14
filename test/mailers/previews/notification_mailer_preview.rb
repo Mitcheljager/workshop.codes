96027bb41445ac7679fc613fd57a571e8d5c90b5
Mitchel <mitchel_jager@hotmail.com>
2026-02-14 14:36:01 +0100

    feat: Add option to receive emails when receiving notifications
    Users can enabled emails in the notifications tab in their profile. This uses a new `send_email_on_notification` on the user model. The checkbox can be enabled for all users, even those logging in with oauth. Previously oauth users could not set an email address because there was no reason to, emails were only used for forgot password. With that I've also added a check to forgot password to check if the user is an oauth user because this reset would make no sense.

---
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
+class NotificationMailerPreview < ActionMailer::Preview
+  def notification
+    NotificationMailer.with(user_id: 0, go_to: "https://workshop.codes/some-notification", content: "I am a notification!").notification
+  end
+end
