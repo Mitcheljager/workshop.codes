28f018b7972a1386d0273a97098d3d9def10ea48
Mitchel <mitchel_jager@hotmail.com>
2026-02-16 02:23:35 +0100

    refactor(admin): Move some repeated queries to methods

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -14,30 +14,22 @@ class Admin::BaseController < ApplicationController
                               .limit(20)
 
     @unique_visits = Rails.cache.fetch("admin_statistics/unique_visits/#{from}-#{short_period}", expires_in: 12.hours) do
-      Statistic.where(content_type: :unique_visit)
-               .where("on_date > ?", from)
-               .pluck(:on_date, :value)
-               .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
-               .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
+      get_statistics(:unique_visit, from, short_period)
     end
 
-    @unique_copies = Rails.cache.fetch("admin_statistics/unique_visits/#{from}-#{short_period}", expires_in: 12.hours) do
-      Statistic.where(content_type: :unique_copies)
-               .where("on_date > ?", from)
-               .pluck(:on_date, :value)
-               .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
-               .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
+    @unique_copies = Rails.cache.fetch("admin_statistics/unique_copies/#{from}-#{short_period}", expires_in: 12.hours) do
+      get_statistics(:unique_copies, from, short_period)
     end
 
-    # This is ugly as hell
     top_pages_events = Rails.cache.fetch("admin_events/top_pages", expires_in: 1.hour) do
-      Ahoy::Event.where(name: "Page View").where("time > ?", 24.hours.ago).pluck(:properties)
+      get_events("Page View")
     end
 
     top_posts_events = Rails.cache.fetch("admin_events/top_posts", expires_in: 1.hour) do
-      Ahoy::Event.where(name: "Copy Code").where("time > ?", 24.hours.ago).pluck(:properties)
+      get_events("Copy Code")
     end
 
+    # This is ugly as hell
     @top_posts_by_views = top_posts_events.group_by do |props|
       props["id"]
     end.map do |id, items| {
@@ -51,5 +43,19 @@ class Admin::BaseController < ApplicationController
       url:,
       count: items.size
     } end.sort_by { |item| -item[:count] }.first(10)
+  end
+
+  private
+
+  def get_statistics(content_type, from, short_period)
+    Statistic.where(content_type:)
+             .where("on_date > ?", from)
+             .pluck(:on_date, :value)
+             .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
+             .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
+  end
+
+  def get_events(name)
+    Ahoy::Event.where(name:).where("time > ?", 24.hours.ago).pluck(:properties)
   end
 end
