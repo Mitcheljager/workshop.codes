15509ddd71a3552f1b6c93bfc7302b79502288d9
Mitchel <mitchel_jager@hotmail.com>
2026-02-16 02:17:47 +0100

    fix(admin): Write cache key per query
    It didn't actually return anything before and it ignored the time period, whoops

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -13,37 +13,43 @@ class Admin::BaseController < ApplicationController
                               .order(created_at: :desc)
                               .limit(20)
 
-    Rails.cache.fetch("admin_statistics", expires_in: 12.hours) do
-      @unique_visits = Statistic.where(content_type: :unique_visit)
-                                .where("on_date > ?", from)
-                                .pluck(:on_date, :value)
-                                .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
-                                .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
-
-      @unique_copies = Statistic.where(content_type: :unique_copies)
-                                .where("on_date > ?", from)
-                                .pluck(:on_date, :value)
-                                .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
-                                .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
+    @unique_visits = Rails.cache.fetch("admin_statistics/unique_visits/#{from}-#{short_period}", expires_in: 12.hours) do
+      Statistic.where(content_type: :unique_visit)
+               .where("on_date > ?", from)
+               .pluck(:on_date, :value)
+               .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
+               .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
     end
 
-    Rails.cache.fetch("admin_events", expires_in: 1.hour) do
-      # This is ugly as hell
-      top_pages_events = Ahoy::Event.where(name: "Page View").where("time > ?", 24.hours.ago).pluck(:properties)
-      @top_pages_by_views = top_pages_events.group_by do |props|
-        props["url"]
-      end.map do |url, items| {
-        url:,
-        count: items.size
-      } end.sort_by { |item| -item[:count] }.first(10)
-
-      top_posts_events = Ahoy::Event.where(name: "Copy Code").where("time > ?", 24.hours.ago).pluck(:properties)
-      @top_posts_by_views = top_posts_events.group_by do |props|
-        props["id"]
-      end.map do |id, items| {
-        id:,
-        count: items.size
-      } end.sort_by { |item| -item[:count] }.first(10)
+    @unique_copies = Rails.cache.fetch("admin_statistics/unique_visits/#{from}-#{short_period}", expires_in: 12.hours) do
+      Statistic.where(content_type: :unique_copies)
+               .where("on_date > ?", from)
+               .pluck(:on_date, :value)
+               .group_by { |on_date, _| short_period ? on_date.to_date : on_date.beginning_of_week }
+               .map do |week_start, rows| { date: week_start.strftime("%Y-%m-%d"), value: rows.sum { |_, v| v } } end
     end
+
+    # This is ugly as hell
+    top_pages_events = Rails.cache.fetch("admin_events/top_pages", expires_in: 1.hour) do
+      Ahoy::Event.where(name: "Page View").where("time > ?", 24.hours.ago).pluck(:properties)
+    end
+
+    top_posts_events = Rails.cache.fetch("admin_events/top_posts", expires_in: 1.hour) do
+      Ahoy::Event.where(name: "Copy Code").where("time > ?", 24.hours.ago).pluck(:properties)
+    end
+
+    @top_posts_by_views = top_posts_events.group_by do |props|
+      props["id"]
+    end.map do |id, items| {
+      id:,
+      count: items.size
+    } end.sort_by { |item| -item[:count] }.first(10)
+
+    @top_pages_by_views = top_pages_events.group_by do |props|
+      props["url"]
+    end.map do |url, items| {
+      url:,
+      count: items.size
+    } end.sort_by { |item| -item[:count] }.first(10)
   end
 end
