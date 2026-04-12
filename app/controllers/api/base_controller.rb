ddfb634301673879cde1aef308cd81fc44bdca58
Mitchel <mitchel_jager@hotmail.com>
2026-04-12 15:16:52 +0200

    fix(api): Set request headers in api endpoint

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,7 @@
 class Api::BaseController < ApplicationController
   before_action :set_request_format
   before_action :set_request_headers
+  before_action :set_cache_headers
 
   def index
     render json: { message: "Hey!" }
@@ -12,7 +13,7 @@ class Api::BaseController < ApplicationController
     request.format = :json
   end
 
-  def set_request_headers
+  def set_cache_headers
     headers["Cache-Control"] = "public, max-age=#{10.minutes}"
   end
 end
