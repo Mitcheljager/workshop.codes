64a6aeb7e8abead1b90e2eb6f47b5ebff4262595
Mitchel <mitchel_jager@hotmail.com>
2026-04-19 01:56:31 +0200

    fix: Update left over controls default values

---
+++ db/migrate/20260418235425_change_post_controls_default_left_over_value.rb
@@ -0,0 +1,9 @@
+class ChangePostControlsDefaultLeftOverValue < ActiveRecord::Migration[7.1]
+  def up
+    Post.where(controls: "{}").update_all(controls: "[]")
+  end
+
+  def down
+    Post.where(controls: "[]").update_all(controls: "{}")
+  end
+end
