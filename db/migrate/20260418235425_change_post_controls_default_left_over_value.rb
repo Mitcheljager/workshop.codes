0ac43637117d95b79c8d05ae66219f0eaeb70583
Mitchel <mitchel_jager@hotmail.com>
2026-04-19 12:24:40 +0200

    chore: Remove down from migration
    That's a bit much

--- db/migrate/20260418235425_change_post_controls_default_left_over_value.rb
+++ db/migrate/20260418235425_change_post_controls_default_left_over_value.rb
@@ -2,8 +2,4 @@ class ChangePostControlsDefaultLeftOverValue < ActiveRecord::Migration[7.1]
   def up
     Post.where(controls: "{}").update_all(controls: "[]")
   end
-
-  def down
-    Post.where(controls: "[]").update_all(controls: "{}")
-  end
 end
