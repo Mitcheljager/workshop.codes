dbf355424d2fc4eeaf568a921d96aeafe092a03f
Mitchel <mitchel_jager@hotmail.com>
2026-04-24 00:12:57 +0200

    feat(admin): Add averages to status chart
    Also moved from seconds to milliseconds

--- app/javascript/src/get-status.ts
+++ app/javascript/src/get-status.ts
@@ -7,6 +7,7 @@ export function render(): void {
 
 async function get(): Promise<void> {
   const chart = document.querySelector<HTMLElement>("[data-svelte-component='StatusChart']")
+  const averageElement = document.querySelector<HTMLElement>("[data-role='status-average']")!
 
   if (!chart) return
 
@@ -16,8 +17,13 @@ async function get(): Promise<void> {
     const props = JSON.parse(chart.dataset.svelteProps!)
 
     props.labels = json.map((i: any) => i.datetime)
-    props.values = json.map((i: any) => i.response_time)
+    props.values = json.map((i: any) => Math.round(i.response_time * 1000))
     chart.dataset.svelteProps = JSON.stringify(props)
+
+    const averageTotal = props.values.reduce((t: number, c: number) => t + c, 0) / props.values.length
+    const averageLastCount = Math.min(100, props.values.length)
+    const averageLast = props.values.slice(-averageLastCount).reduce((t: number, c: number) => t + c, 0) / averageLastCount
+    averageElement.innerText = `Total average: ${Math.round(averageTotal)}ms - Last 100 average: ${Math.round(averageLast)}ms`
 
     initializeSvelteComponent("StatusChart", LinkedChart)
   } catch (error: any) {
