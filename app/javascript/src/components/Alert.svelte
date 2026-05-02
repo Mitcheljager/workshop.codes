aa2b277669dfa2f8f3aabd689609bf1c0af299a7
Mitchel <mitchel_jager@hotmail.com>
2026-05-03 00:04:52 +0200

    feat: Pause alerts while hovering alert

--- app/javascript/src/components/Alert.svelte
+++ app/javascript/src/components/Alert.svelte
@@ -7,16 +7,33 @@
 
   const timer = 5000
 
-  let timeout
+  let timeout: ReturnType<typeof setTimeout> | null = $state(null)
+  let playTime: number = $state(0)
+  let remainingTime: number = $state(timer)
+  let paused: boolean = $state(true)
 
   onMount(() => {
-    setTimeout(onclose, timer)
+    play()
 
     return () => { if (timeout) clearTimeout(timeout) }
   })
+
+  function pause(): void {
+    paused = true
+    remainingTime = remainingTime - (new Date().getTime() - playTime)
+
+    if (timeout) clearTimeout(timeout)
+  }
+
+  function play(): void {
+    paused = false
+    playTime = new Date().getTime()
+    timeout = setTimeout(onclose, remainingTime)
+  }
 </script>
 
-<div class="alert {type} static" aria-atomic="true" aria-live={type === "error" ? "assertive" : "polite"}>
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="alert {type} static" class:alert--paused={paused} aria-atomic="true" aria-live={type === "error" ? "assertive" : "polite"} onmouseenter={pause} onmouseleave={play}>
   <p class="m-0">{text}</p>
 
   <button class="button p-0 pl-1/16 pr-1/16 text-white" onclick={() => onclose()} aria-label="Close alert">✕</button>
