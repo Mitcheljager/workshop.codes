3b53fd5f661c1cd0dbcb23e7f133886d2a4136d5
Mitcheljager <mitchel_jager@hotmail.com>
2026-09-06 01:45:39 +0200

    chore: Remove duplicate and wrong config option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files (see config/storage.yml for options)
   config.active_storage.service = :digitalocean
 
   # Mount Action Cable outside main process or domain
